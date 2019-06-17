4dafcb4dd522efac38b970b2d4897dd6eab31bb6
sharath <sharaths.kashyap@gmail.com>
2019-06-17 16:56:59 +0530

    App Routing Issue Resolved

--- src/app/language-settings/language-settings.module.ts
+++ src/app/language-settings/language-settings.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageSettingsPage } from '../language-settings/language-settings';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule} from '@angular/forms';
-import { CommonModule} from '@angular/common';
+
 // import { OnboardingPageModule } from '../onboarding/onboarding.module';
 
 @NgModule({
   declarations: [LanguageSettingsPage],
   imports: [
-    // OnboardingPageModule,
-    IonicModule,
-    FormsModule,
     CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: LanguageSettingsPage
+      }
+    ]),
+    // OnboardingPageModule,
     TranslateModule.forChild()
   ],
   exports: [LanguageSettingsPage]
--- src/app/user-type-selection/user-type-selection.module.ts
+++ src/app/user-type-selection/user-type-selection.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
 // import { IonicPageModule } from '@ionic/angular';
 import { UserTypeSelectionPage } from './user-type-selection';
 import { TranslateModule } from '@ngx-translate/core';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule} from '@angular/forms';
-import { CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     UserTypeSelectionPage
   ],
   imports: [
-    // IonicPageModule.forChild(UserTypeSelectionPage),
-    IonicModule,
-    FormsModule,
     CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: UserTypeSelectionPage
+      }
+    ]),
+    // IonicPageModule.forChild(UserTypeSelectionPage),
     TranslateModule.forChild()
   ],
   exports: [
