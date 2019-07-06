8382347a83c58140d26f5a906c1086e5a7380041
diptesh6501 <diptesh1993@gmail.com>
2019-07-06 08:55:33 -0400

    added course batches component

---
+++ src/app/course-batches/course-batches.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CourseBatchesPage } from './course-batches.page';
+import { TranslateModule } from '@ngx-translate/core';
+import { PipesModule } from '@app/pipes/pipes.module';
+import { DirectivesModule } from '@app/directives/directives.module';
+import { ComponentsModule } from '../components/components.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CourseBatchesPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes),
+    TranslateModule.forChild(),
+    PipesModule,
+    DirectivesModule,
+    ComponentsModule
+  ],
+  declarations: [CourseBatchesPage]
+})
+export class CourseBatchesPageModule {}
