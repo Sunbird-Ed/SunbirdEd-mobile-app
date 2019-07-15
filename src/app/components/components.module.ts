fe00c1da17c24d5a67579e604e18f0254e3bc9ab
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-07-15 12:22:51 +0530

    Issue #SB-13331 feat: Added Player pages

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,31 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ApplicationHeaderComponent } from './application-header/application-header.component';
-import { SignInCardComponent} from './sign-in-card/sign-in-card.component';
-import {TextBookCardComponent} from './text-book-card/text-book-card.component';
-import {NewCourseCardComponent} from './new-course-card/new-course-card.component';
+import { SignInCardComponent } from './sign-in-card/sign-in-card.component';
+import { TextBookCardComponent } from './text-book-card/text-book-card.component';
+import { NewCourseCardComponent } from './new-course-card/new-course-card.component';
 import { PipesModule } from '../../pipes/pipes.module';
-import {ViewAllCardComponent} from './view-all-card/view-all-card.component';
-import {ViewMoreCardComponent} from './view-more-card/view-more-card.component';
-import {PbHorizontalComponent} from './pb-horizontal/pb-horizontal.component';
+import { ViewAllCardComponent } from './view-all-card/view-all-card.component';
+import { ViewMoreCardComponent } from './view-more-card/view-more-card.component';
+import { PbHorizontalComponent } from './pb-horizontal/pb-horizontal.component';
 import { CourseCardComponent } from './cards/coursecard/coursecard.component';
 import { SbGenericPopoverComponent } from './popups/sb-generic-popover/sb-generic-popover.component';
 import { SbNoNetworkPopupComponent } from './popups/sb-no-network-popup/sb-no-network-popup.component';
 import { SbPopoverComponent } from './popups/sb-popover/sb-popover.component';
-import { CollectionChildComponent} from './collection-child/collection-child.component';
-import {ContentActionsComponent} from './content-actions/content-actions.component';
-import {ContentRatingAlertComponent} from './content-rating-alert/content-rating-alert.component';
+import { CollectionChildComponent } from './collection-child/collection-child.component';
+import { ContentActionsComponent } from './content-actions/content-actions.component';
+import { ContentRatingAlertComponent } from './content-rating-alert/content-rating-alert.component';
 import { IonicRatingModule, RatingComponent } from 'ionic4-rating';
-import {DetailCardComponent} from './detail-card/detail-card.component';
+import { DetailCardComponent } from './detail-card/detail-card.component';
 import { FileSizePipe } from '@app/pipes/file-size/file-size';
-import {BookmarkComponent} from './bookmark/bookmark.component';
-import {DialogPopupComponent} from './dialog-popup/dialog-popup.component';
-import {SbDownloadPopupComponent} from './popups/sb-download-popup/sb-download-popup.component';
+import { BookmarkComponent } from './bookmark/bookmark.component';
+import { DialogPopupComponent } from './dialog-popup/dialog-popup.component';
+import { SbDownloadPopupComponent } from './popups/sb-download-popup/sb-download-popup.component';
 import { UpgradePopoverComponent } from './popups/upgrade-popover/upgrade-popover.component';
+import { EditContactDetailsPopupComponent } from './popups/edit-contact-details-popup/edit-contact-details-popup.component';
+import { EditContactVerifyPopupComponent } from './popups/edit-contact-verify-popup/edit-contact-verify-popup.component';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 
 @NgModule({
@@ -47,11 +50,14 @@ import { UpgradePopoverComponent } from './popups/upgrade-popover/upgrade-popove
     BookmarkComponent,
     DialogPopupComponent,
     SbDownloadPopupComponent,
-    UpgradePopoverComponent
+    UpgradePopoverComponent,
+    EditContactDetailsPopupComponent,
+    EditContactVerifyPopupComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     PipesModule,
     TranslateModule.forChild(),
@@ -65,7 +71,9 @@ import { UpgradePopoverComponent } from './popups/upgrade-popover/upgrade-popove
     SbNoNetworkPopupComponent,
     ContentRatingAlertComponent,
     DialogPopupComponent,
-    UpgradePopoverComponent
+    UpgradePopoverComponent,
+    EditContactDetailsPopupComponent,
+    EditContactVerifyPopupComponent
   ],
   exports: [
     ApplicationHeaderComponent,
@@ -86,8 +94,11 @@ import { UpgradePopoverComponent } from './popups/upgrade-popover/upgrade-popove
     BookmarkComponent,
     DialogPopupComponent,
     SbDownloadPopupComponent,
-    UpgradePopoverComponent
+    UpgradePopoverComponent,
+    EditContactDetailsPopupComponent,
+    EditContactDetailsPopupComponent
+
   ],
-  providers: [FileSizePipe]
+  providers: [FileSizePipe, Keyboard]
 })
 export class ComponentsModule { }
