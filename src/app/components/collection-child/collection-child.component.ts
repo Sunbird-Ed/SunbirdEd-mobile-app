24bef78c630ad301bcce31b2f8d2ce5626e76f9f
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-07-25 21:34:03 +0530

    Issue #SB-13335 feat: Fixed pages, removed unwanted code

--- src/app/components/collection-child/collection-child.component.ts
+++ src/app/components/collection-child/collection-child.component.ts
@@ -1,14 +1,6 @@
-import { Component, Input, NgZone, AfterViewInit, OnInit } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
-// migration-TODO
-// import { EnrolledCourseDetailsPage } from '@app/pages/enrolled-course-details';
+import { Component, Input, NgZone, OnInit } from '@angular/core';
 import { ContentType, MimeType, RouterLinks } from '@app/app/app.constant';
-
-import { CollectionDetailEtbPage } from '../../collection-detail-etb/collection-detail-etb.page';
-// migration-TODO
-// import { ContentDetailsPage } from '@app/pages/content-details/content-details';
-
-import { CommonUtilService, ComingSoonMessageService } from '../../../services';
+import { CommonUtilService, ComingSoonMessageService } from '@app/services';
 import { PopoverController } from '@ionic/angular';
 import { SbGenericPopoverComponent } from '../popups/sb-generic-popover/sb-generic-popover.component';
 import { Content } from 'sunbird-sdk';
@@ -20,8 +12,8 @@ import { Router, NavigationExtras } from '@angular/router';
   styleUrls: ['./collection-child.component.scss'],
 })
 export class CollectionChildComponent implements OnInit {
-//   migration-TODO : remove unnecessary
-//   cardData: any;
+  //   migration-TODO : remove unnecessary
+  //   cardData: any;
   @Input() childData: Content;
   @Input() index: any;
   @Input() depth: any;
@@ -30,17 +22,12 @@ export class CollectionChildComponent implements OnInit {
   @Input() breadCrumb: any;
 
   constructor(
-      private navCtrl: NavController,
-      private zone: NgZone,
-    //   migration-TODO : remove unnecessary
-    //   private navParams: NavParams,
-      private commonUtilService: CommonUtilService,
-      private popoverCtrl: PopoverController,
-      private comingSoonMessageService: ComingSoonMessageService,
-      private router: Router
-  ) {
-    //   this.cardData = this.navParams.get('content');
-    }
+    private zone: NgZone,
+    private commonUtilService: CommonUtilService,
+    private popoverCtrl: PopoverController,
+    private comingSoonMessageService: ComingSoonMessageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
@@ -49,74 +36,74 @@ export class CollectionChildComponent implements OnInit {
     //   migration-TODO : remove unnecessary
     //   const stateData = this.navParams.get('contentState');
 
-      this.zone.run(() => {
-          if (content.contentType === ContentType.COURSE) {
-              //   migration-TODO : remove unnecessary
-              // this.navCtrl.push(EnrolledCourseDetailsPage, {
-              //     content: content,
-              //     depth: depth,
-              //     contentState: stateData,
-              //     corRelation: this.corRelationList,
-              //     breadCrumb: this.breadCrumb
-              // });
-          } else if (content.mimeType === MimeType.COLLECTION) {
-              this.isDepthChild = true;
-              const collectionDetailsParams: NavigationExtras = {
-                state: {
-                    content: content,
-                    depth: depth,
-                    // migration-TODO : remove unnece
-                    // contentState: stateData,
-                    corRelation: this.corRelationList,
-                    breadCrumb: this.breadCrumb
-                }
-            };
-              this.router.navigate([RouterLinks.COLLECTION_DETAIL_ETB], collectionDetailsParams);
-              //   migration-TODO : remove unnecessary
-              // this.navCtrl.push(CollectionDetailsEtbPage, {
-              //     content: content,
-              //     depth: depth,
-              //     contentState: stateData,
-              //     corRelation: this.corRelationList,
-              //     breadCrumb: this.breadCrumb
-              // });
-          } else {
-            const contentDetailsParams: NavigationExtras = {
-                state: {
-                    isChildContent: true,
-                    content: content,
-                    depth: depth,
-                    // migration-TODO : remove unnece
-                    // contentState: stateData,
-                    corRelation: this.corRelationList,
-                    breadCrumb: this.breadCrumb
-                }
-            };
-            this.router.navigate([RouterLinks.CONTENT_DETAILS], contentDetailsParams);
+    this.zone.run(() => {
+      if (content.contentType === ContentType.COURSE) {
+        //   migration-TODO : remove unnecessary
+        // this.navCtrl.push(EnrolledCourseDetailsPage, {
+        //     content: content,
+        //     depth: depth,
+        //     contentState: stateData,
+        //     corRelation: this.corRelationList,
+        //     breadCrumb: this.breadCrumb
+        // });
+      } else if (content.mimeType === MimeType.COLLECTION) {
+        this.isDepthChild = true;
+        const collectionDetailsParams: NavigationExtras = {
+          state: {
+            content,
+            depth,
+            // migration-TODO : remove unnece
+            // contentState: stateData,
+            corRelation: this.corRelationList,
+            breadCrumb: this.breadCrumb
           }
-      });
+        };
+        this.router.navigate([RouterLinks.COLLECTION_DETAIL_ETB], collectionDetailsParams);
+        //   migration-TODO : remove unnecessary
+        // this.navCtrl.push(CollectionDetailsEtbPage, {
+        //     content: content,
+        //     depth: depth,
+        //     contentState: stateData,
+        //     corRelation: this.corRelationList,
+        //     breadCrumb: this.breadCrumb
+        // });
+      } else {
+        const contentDetailsParams: NavigationExtras = {
+          state: {
+            isChildContent: true,
+            content,
+            depth,
+            // migration-TODO : remove unnece
+            // contentState: stateData,
+            corRelation: this.corRelationList,
+            breadCrumb: this.breadCrumb
+          }
+        };
+        this.router.navigate([RouterLinks.CONTENT_DETAILS], contentDetailsParams);
+      }
+    });
   }
 
 
   async showComingSoonPopup(childData: any) {
-      const message = await this.comingSoonMessageService.getComingSoonMessage(childData);
-      if (childData.contentData.mimeType === MimeType.COLLECTION && !childData.children) {
-          const popover = await  this.popoverCtrl.create({
-            component: SbGenericPopoverComponent,
-            componentProps: {
-              sbPopoverHeading: this.commonUtilService.translateMessage('CONTENT_COMMING_SOON'),
-              sbPopoverMainTitle: message ? this.commonUtilService.translateMessage(message) :
-                  this.commonUtilService.translateMessage('CONTENT_IS_BEEING_ADDED') + childData.contentData.name,
-              actionsButtons: [
-                  {
-                      btntext: this.commonUtilService.translateMessage('OKAY'),
-                      btnClass: 'popover-color'
-                  }
-              ],
-          },
-                  cssClass: 'sb-popover warning',
-              });
-          popover.present();
-      }
+    const message = await this.comingSoonMessageService.getComingSoonMessage(childData);
+    if (childData.contentData.mimeType === MimeType.COLLECTION && !childData.children) {
+      const popover = await this.popoverCtrl.create({
+        component: SbGenericPopoverComponent,
+        componentProps: {
+          sbPopoverHeading: this.commonUtilService.translateMessage('CONTENT_COMMING_SOON'),
+          sbPopoverMainTitle: message ? this.commonUtilService.translateMessage(message) :
+            this.commonUtilService.translateMessage('CONTENT_IS_BEEING_ADDED') + childData.contentData.name,
+          actionsButtons: [
+            {
+              btntext: this.commonUtilService.translateMessage('OKAY'),
+              btnClass: 'popover-color'
+            }
+          ],
+        },
+        cssClass: 'sb-popover warning',
+      });
+      popover.present();
+    }
   }
 }
