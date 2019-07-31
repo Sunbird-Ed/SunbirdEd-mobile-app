bfd6de6407bb0b6d40a0f5af6e04e46c252d7830
diptesh6501 <diptesh1993@gmail.com>
2019-07-31 12:36:43 +0530

    Issue #SB-13332 course module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -80,7 +80,6 @@ const routes: Routes = [
     path: `${RouterLinks.MENU_LANGUAGE_SETTING}/:isFromSettings`,
     loadChildren: './language-settings/language-settings.module#LanguageSettingsModule'
   },
-  { path: RouterLinks.ONBOARDING, loadChildren: './onboarding/onboarding.module#OnboardingPageModule' },
   { path: 'filters', loadChildren: './search/filters/filters.module#FiltersPageModule' },
 ];
 
