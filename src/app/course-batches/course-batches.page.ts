56285d99c3dd7965b7ccf6764bf82405e82651b8
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-09-13 12:10:22 +0530

    Issue #SB-14505 feat: open first dropdown

--- src/app/course-batches/course-batches.page.ts
+++ src/app/course-batches/course-batches.page.ts
@@ -131,56 +131,15 @@ export class CourseBatchesPage implements OnInit {
   }
 
   ionViewWillLeave() {
-    if(this.backButtonFunc) {
+    if (this.backButtonFunc) {
       this.backButtonFunc.unsubscribe();
-    } 
+    }
   }
   /**
    * Enroll logged-user into selected batch
    *
    * @param {any} item contains details of select batch
    */
-  async enrollIntoBatch1(item: Batch) {
-    const enrollCourseRequest: EnrollCourseRequest = {
-      batchId: item.id,
-      courseId: item.courseId,
-      userId: this.userId,
-      batchStatus: item.status
-    };
-    const loader = await this.commonUtilService.getLoader();
-    await loader.present();
-    const reqvalues = new Map();
-    reqvalues['enrollReq'] = enrollCourseRequest;
-    this.telemetryGeneratorService.generateInteractTelemetry(InteractType.TOUCH,
-      InteractSubtype.ENROLL_CLICKED,
-      Environment.HOME,
-      PageId.COURSE_BATCHES, undefined,
-      reqvalues);
-
-    this.courseService.enrollCourse(enrollCourseRequest).toPromise()
-      .then((data: boolean) => {
-        this.zone.run(async () => {
-          this.commonUtilService.showToast(this.commonUtilService.translateMessage('COURSE_ENROLLED'));
-          this.events.publish(EventTopics.ENROL_COURSE_SUCCESS, {
-            batchId: item.id,
-            courseId: item.courseId
-          });
-          await loader.dismiss();
-          // this.navCtrl.pop();
-          this.location.back();
-
-        });
-      }, (error) => {
-        this.zone.run(() => {
-          if (error && error.code === 'NETWORK_ERROR') {
-            this.commonUtilService.showToast(this.commonUtilService.translateMessage('ERROR_NO_INTERNET_MESSAGE'));
-          } else if (error && error.response
-            && error.response.body && error.response.body.params && error.response.body.params.err === 'USER_ALREADY_ENROLLED_COURSE') {
-            this.commonUtilService.showToast(this.commonUtilService.translateMessage('ALREADY_ENROLLED_COURSE'));
-          }
-        });
-      });
-  }
 
   async enrollIntoBatch(item: Batch) {
     if (this.isGuestUser) {
@@ -245,20 +204,14 @@ export class CourseBatchesPage implements OnInit {
             btnClass: 'popover-color'
           },
         ]
-        // handler : this.handleEnrollCoursePopup.bind(this)
       },
       cssClass: 'sb-popover info',
     });
-    // if (this.isGuestUser) {
     await confirm.present();
     const { data } = await confirm.onDidDismiss();
     if (data && data.canDelete) {
       this.loginHandlerService.signIn();
     }
-    // } else {
-    //   console.log('loggedin user');
-    //   this.navigateToBatchListPage();
-    // }
   }
   /**
    * Get logged-user id. User id is needed to enroll user into batch.
