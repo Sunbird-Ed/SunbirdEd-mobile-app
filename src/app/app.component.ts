d5787339b817a30c808e32d7c4091707bef7a908
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2019-09-16 16:08:10 +0530

    Issue #SB-14700 fix: Splash screen delayed till the onboarding navigation completes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -439,8 +439,10 @@ export class AppComponent implements OnInit, AfterViewInit {
       }
     }
 
-    splashscreen.markImportDone();
-    splashscreen.hide();
+    setTimeout(() => {
+      splashscreen.markImportDone();
+      splashscreen.hide();
+    }, 2500);
   }
 
   private autoSyncTelemetry() {
