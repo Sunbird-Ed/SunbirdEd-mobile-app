f5a6f4a775575e3a436691a5501401afa203e8d3
Dhirenaade <dhirenaade@gmail.com>
2019-11-06 15:12:53 +0530

    Issue #SB-14974 fix:resolve generate Interact telemetry for filter search option from librarypage

--- src/app/components/filteroption/filteroption.component.ts
+++ src/app/components/filteroption/filteroption.component.ts
@@ -1,6 +1,10 @@
 import { Component , ViewEncapsulation  } from '@angular/core';
 import { NavParams, PopoverController, Platform } from '@ionic/angular';
 import { Subscription } from 'rxjs/Subscription';
+import { TelemetryGeneratorService } from '@app/services/telemetry-generator.service';
+import {
+  Environment, InteractSubtype, InteractType, PageId
+} from '@app/services/telemetry-constants';
 @Component({
   selector: 'app-filteroption',
   templateUrl: './filteroption.component.html',
@@ -15,7 +19,8 @@ export class FilteroptionComponent {
   constructor(
     private navParams: NavParams,
     private popCtrl: PopoverController,
-    private platform: Platform
+    private platform: Platform,
+    private telemetryGeneratorService: TelemetryGeneratorService
     ) {
     this.facets = this.navParams.get('facet');
     this.backButtonFunc = this.platform.backButton.subscribeWithPriority(11, () => {
@@ -25,7 +30,14 @@ export class FilteroptionComponent {
   }
 
   confirm() {
+    const values = new Map();
+    values['Option'] = this.facets.name;
+    this.telemetryGeneratorService.generateInteractTelemetry(InteractType.TOUCH,
+      InteractSubtype.APPLY_FILTER_CLICKED,
+      Environment.HOME,
+      PageId.LIBRARY_SEARCH_FILTER,
+      undefined,
+      values);
     this.popCtrl.dismiss();
   }
-
 }
