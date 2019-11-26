97487d15dc81fe6b0416385f691402c0e88d3da9
Ajoymaity <ajoymaity17@gmail.com>
2019-11-26 15:39:51 +0530

    issue #SB-15505 feat: Topic based notification

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -211,6 +211,7 @@ export class PreferenceKey {
     public static readonly DEPLOYMENT_KEY = 'deployment_key';
     public static readonly COURSE_DATA_KEY = 'course_data';
     public static readonly BATCH_DETAIL_KEY = 'batch_detail';
+    public static readonly IS_LOCATION_MANDATORY = 'is_location_mandatory';
     public static readonly TOPICS = 'topics';
 }
 
