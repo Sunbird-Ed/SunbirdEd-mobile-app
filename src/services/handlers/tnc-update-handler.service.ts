31cf2c2ddfc79c80e7155fbfaca4d4b512821607
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2019-12-11 18:36:53 +0530

    Issue #SB-16489 feat: Reverting BMC changes of 2.6.0 in 2.6.5

--- src/services/handlers/tnc-update-handler.service.ts
+++ src/services/handlers/tnc-update-handler.service.ts
@@ -10,7 +10,6 @@ import { Router, NavigationExtras } from '@angular/router';
 import { CommonUtilService } from '../common-util.service';
 import { FormAndFrameworkUtilService } from '../formandframeworkutil.service';
 import { ExternalIdVerificationService } from '../externalid-verification.service';
-import { AppGlobalService } from '../app-global-service.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +17,6 @@ import { AppGlobalService } from '../app-global-service.service';
 export class TncUpdateHandlerService {
 
   modal: any;
-  private profile: any;
   constructor(
     @Inject('PROFILE_SERVICE') private profileService: ProfileService,
     @Inject('AUTH_SERVICE') private authService: AuthService,
@@ -26,8 +24,7 @@ export class TncUpdateHandlerService {
     private formAndFrameworkUtilService: FormAndFrameworkUtilService,
     private modalCtrl: ModalController,
     private router: Router,
-    private externalIdVerificationService: ExternalIdVerificationService,
-    private appGlobalService: AppGlobalService,
+    private externalIdVerificationService: ExternalIdVerificationService
   ) { }
 
   public async checkForTncUpdate(): Promise<boolean> {
@@ -46,9 +43,31 @@ export class TncUpdateHandlerService {
         .then((profile) => {
           if (!this.hasProfileTncUpdated(profile)) {
             if (this.commonUtilService.networkInfo.isNetworkAvailable) {
-              this.checkBmc(profile);
-              resolve(false);
-              return;
+              this.formAndFrameworkUtilService.getCustodianOrgId()
+                .then((custodianOrgId: string) => {
+                  const isCustodianOrgId = profile.rootOrg.rootOrgId === custodianOrgId;
+
+                  if (isCustodianOrgId
+                    && !this.commonUtilService.isUserLocationAvalable(profile)) {
+                    const navigationExtras: NavigationExtras = {
+                      state: {
+                        isShowBackButton: false
+                      }
+                    };
+                    this.router.navigate(['/', RouterLinks.DISTRICT_MAPPING], navigationExtras)
+                      .then(() => resolve(false));
+                    return;
+                  } else {
+                    this.externalIdVerificationService.showExternalIdVerificationPopup();
+                    resolve(false);
+                    return;
+                  }
+                })
+                .catch((error) => {
+                  this.externalIdVerificationService.showExternalIdVerificationPopup();
+                  console.error('Error:', error);
+                  reject();
+                });
             }
             resolve(false);
             return;
@@ -111,71 +130,4 @@ export class TncUpdateHandlerService {
       return await this.modal.dismiss();
     }
   }
-
-  private async checkBmc(profile) {
-    const userDetails = await this.appGlobalService.getCurrentUser();
-    if (userDetails && userDetails.board && userDetails.grade && userDetails.medium && userDetails.syllabus &&
-      !userDetails.board.length && !userDetails.grade.length && !userDetails.medium.length && !userDetails.syllabus.length) {
-      this.preRequirementToBmcNavigation(profile.userId);
-    } else {
-      this.checkDistrictMapping(profile);
-    }
-  }
-
-  private async preRequirementToBmcNavigation(userId) {
-    const serverProfile = await this.profileService.getServerProfilesDetails({
-      userId,
-      requiredFields: ProfileConstants.REQUIRED_FIELDS,
-      from: CachedItemRequestSourceFrom.SERVER
-    }).toPromise();
-
-    const userprofile = await this.profileService.getActiveSessionProfile({
-      requiredFields: ProfileConstants.REQUIRED_FIELDS
-    }).toPromise();
-
-    this.navigateToBmc(serverProfile, userprofile)
-  }
-
-  private async navigateToBmc(serverProfile, userprofile) {
-    this.formAndFrameworkUtilService.updateLoggedInUser(serverProfile, userprofile)
-      .then(async (value) => {
-        this.router.navigate([`/${RouterLinks.PROFILE}/${RouterLinks.CATEGORIES_EDIT}`], {
-          state: {
-            hasFilledLocation: this.commonUtilService.isUserLocationAvalable(serverProfile),
-            showOnlyMandatoryFields: true,
-            profile: value['profile'],
-            isRootPage: true
-          }
-        });
-    });
-  }
-
-  private checkDistrictMapping(profile) {
-    this.formAndFrameworkUtilService.getCustodianOrgId()
-      .then((custodianOrgId: string) => {
-        const isCustodianOrgId = profile.rootOrg.rootOrgId === custodianOrgId;
-        if (isCustodianOrgId
-          && !this.commonUtilService.isUserLocationAvalable(profile)) {
-          this.navigateToDistrictMapping();
-          return;
-        } else {
-          this.externalIdVerificationService.showExternalIdVerificationPopup();
-          return;
-        }
-      })
-      .catch((error) => {
-        this.externalIdVerificationService.showExternalIdVerificationPopup();
-        console.error('Error:', error);
-      });
-  }
-
-  private navigateToDistrictMapping(){
-    const navigationExtras: NavigationExtras = {
-      state: {
-        isShowBackButton: false
-      }
-    };
-    this.router.navigate(['/', RouterLinks.DISTRICT_MAPPING], navigationExtras);
-  }
-
 }
