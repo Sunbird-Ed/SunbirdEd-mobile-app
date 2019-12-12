643df5dcf01cc14eb088d25ccb95321c511a1881
Ajoymaity <ajoymaity17@gmail.com>
2019-12-12 16:03:07 +0530

    Issue #SB-16091 feat: Script writting for copy content player

--- copyContentPlayer.js
+++ copyContentPlayer.js
@@ -1,21 +1,25 @@
 const path = require('path');
 const fs = require('fs-extra');
-const srcPath = '../content-player';
-const destinationPath = '../www/content-player';
+const srcPath = 'node_modules/@project-sunbird/content-player';
+const destinationPath = 'www/content-player';
 
 fs.readdir(srcPath, function (err, files) {
     if (err) {
         // return console.log('Unable to scan directory: ' + err);
     }
     files.forEach(function (file) {
-        if (file === 'canvas-interface.js' || file === 'canvas-telemetry-interface.js'
-            || file === 'polyfills.js' || file === 'preview.html') {
-             //   console.log(file);
+        if (file !== 'node_modules' && file !== 'preview.html' && file !== 'README.md'
+            && file !== 'chunks' && file !== 'preview_cdn.html' && file !== 'package.json') {
+            //    console.log(file);
             fs.copy(path.join(srcPath, file), path.join(destinationPath, file), (err) => {
                 if (err) throw err;
-            //    console.log('File was copied to destination');
+              //  console.log('File was copied to destination');
             });
         }
     });
 });
+
+
+
+
 
