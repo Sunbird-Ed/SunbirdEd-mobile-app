4574da2a391a33306a62247c9a8653a8de1f95d9
Ajoymaity <ajoymaity17@gmail.com>
2019-12-13 18:51:27 +0530

    Issue #SB-16091 fix: Fixed copyContentPlayer file location

--- scripts/copyContentPlayer.js
+++ scripts/copyContentPlayer.js
@@ -1,51 +1,46 @@
 const path = require('path');
 const fs = require('fs-extra');
-var srcPath;
-var destinationPath;
 
 function copyFromNodeModule(src, dist) {
-    fs.readdir(src, function (err, files) {
-        if (err) {
-            // return console.log('Unable to scan directory: ' + err);
-        }
+    try {
+        const files = fs.readdirSync(src);
         files.forEach(function (file) {
-            if (file !== 'node_modules' && file !== 'preview.html' && file !== 'README.md'
-                && file !== 'chunks' && file !== 'preview_cdn.html' && file !== 'package.json') {
-                //    console.log(file);
-                fs.copy(path.join(src, file), path.join(dist, file), (err) => {
-                    if (err) throw err;
-                    //  console.log('File was copied to destination');
-                });
+            if (['assets', 'coreplugins', 'fonts', 'style.min.1.1.js', 'coreplugins.js',
+             'script.min.1.1.js', 'style.min.1.1.css', 'youtube.html'].includes(file)) {
+                    console.log(file);
+                fs.copySync(path.join(src, file), path.join(dist, file));
             }
         });
-    });
+    } catch(e) {
+        console.error(e);
+    }
+
 }
 
 function copyFromContentPlayer(src, dist) {
-    fs.readdir(src, function (err, files) {
-        if (err) {
-          //  return console.log('Unable to scan directory: ' + err);
-        }
-        files.forEach(function (file) {
-            if (file === 'canvas-interface.js' || file === 'canvas-telemetry-interface.js'
-                || file === 'polyfills.js' || file === 'preview.html') {
-             //   console.log(file);
-                fs.copy(path.join(src, file), path.join(dist, file), (err) => {
-                    if (err) throw err;
-                   // console.log('File was copied to destination');
-                });
-            }
-        });
-    });
+    try {
+        fs.copySync(src, dist);
+    } catch(e) {
+        console.error(e);
+    }
+    
 }
-
 module.exports = function (context) {
-    srcPath = path.join(__dirname, '../node_modules/@project-sunbird/content-player');
-    destinationPath = path.join(__dirname, '../www/content-player');
-    copyFromNodeModule(srcPath, destinationPath);
-    console.log('copy file/folders from node_modules/content-player to www/content-player')
+    var srcPath;
+    var destinationPath;
+
     srcPath = path.join(__dirname, '../content-player');
     destinationPath = path.join(__dirname, '../www/content-player');
     copyFromContentPlayer(srcPath, destinationPath);
-    console.log('copy file/folders from app content-player to www/content-player');
+    console.log('copy completed!');
+
+    srcPath = path.join(__dirname, '../node_modules/@project-sunbird/content-player');
+    destinationPath = path.join(__dirname, '../www/content-player');
+    copyFromNodeModule(srcPath, destinationPath);
+    console.log('copy completed!');
 }
+
+
+
+
+
