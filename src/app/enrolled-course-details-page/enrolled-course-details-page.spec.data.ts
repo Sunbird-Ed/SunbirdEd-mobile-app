af882a163e8918be90564dd068814eaf9ae3b141
Ajoymaity <ajoymaity17@gmail.com>
2019-12-18 14:23:57 +0530

    Issue #SB-16089 feat: Test case for enrolled course details

--- src/app/enrolled-course-details-page/enrolled-course-details-page.spec.data.ts
+++ src/app/enrolled-course-details-page/enrolled-course-details-page.spec.data.ts
@@ -1,4 +1,5 @@
-import { Navigation, UrlTree } from "@angular/router";
+import { Navigation } from '@angular/router';
+import { Content } from 'sunbird-sdk';
 
 export const mockEnrolledData: Partial<Navigation> = {
     id: 7,
@@ -14,6 +15,7 @@ export const mockEnrolledData: Partial<Navigation> = {
                 copyright: 'R2.1.0',
                 subject: 'Biology',
                 channel: '0127870805901967364',
+                isAvailableLocally: false,
                 downloadUrl: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_212911645382959104165/license-check-course_1576128983035_do_212911645382959104165_2.0_spine.ecar',
                 organisation: [
                     'R2.1.0'
@@ -79,71 +81,182 @@ export const mockEnrolledData: Partial<Navigation> = {
     }
 };
 
-export const mockRes = {
-    contentDetailsResponse: {
-        message: 'successful',
-        result: {
-            contentData: {
-                appIcon: 'sample/do_212516141114736640146589/artifact/7fd95b138e3e32dc73cc6c8ce7af59cb_1527849151139.thumb.jpeg',
-                artifactUrl: 'sample146589/mp4_877_1527849257.mp4',
-                attributions: [
-                    'CC'
-                ],
-                audience: [
-                    'Learner'
-                ],
-                me_totalRatings: '3.0',
-                board: 'CBSE',
-                contentDisposition: 'inline',
-                contentEncoding: 'identity',
-                contentType: 'Resource',
-                createdBy: '877',
-                createdOn: '2018-06-01T10:34:17.170+0000',
-                creator: 'Subrat Rath',
-                description: 'Swing around',
-      downloadUrl: 'sample.amazonaws.com/ecar_files/do_212516141114736640146589/swing_1527849415336_do_212516141114736640146589_1.0.ecar',
-                gradeLevel: [
-                    'KG',
-                    'Class 1'
-                ],
-                identifier: 'SAMPLE_ID',
-                language: [
-                    'English'
-                ],
-                lastPublishedOn: '2018-06-01T10:36:55.335+0000',
-                mimeType: 'video/mp4',
-                name: 'Swing',
-                osId: 'org.ekstep.quiz.app',
-                owner: 'Joker',
-                pkgVersion: '1.0',
-                publisher: '',
-                resourceType: 'Practice',
-                size: '2120865.0',
-                status: 'Live',
-                subject: 'English',
-                variants: {
-                    spine: {
-                        ecarUrl: 'sample589_1.0_spine.ecar',
-                        size: 17434
-                    }
-                },
-                versionKey: '1527849415052',
-                me_totalDownloads: '24.0'
+export const contentDetailsResponse: Content = {
+    identifier: 'do_21281258639073280011490',
+    contentData: {
+        ownershipType: [
+            'createdBy'
+        ],
+        copyright: 'Odisha',
+        keywords: [
+            'test'
+        ],
+        subject: 'Physics',
+        downloadUrl: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_21281258639073280011490/sb-13081-2_1564037681733_do_21281258639073280011490_2.0_spine.ecar',
+        channel: '01269936129926758441',
+        organisation: [
+            'Odisha'
+        ],
+        language: [
+            'English',
+            'Kannada'
+        ],
+        variants: {
+            online: {
+                ecarUrl: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_21281258639073280011490/sb-13081-2_1564037681998_do_21281258639073280011490_2.0_online.ecar',
+                size: 14437
             },
-            contentType: 'resource',
-            identifier: 'do_2125161',
-            isAvailableLocally: true,
-            isUpdateAvailable: false,
-            lastUpdatedTime: 0,
-            mimeType: 'video/mp4',
-            referenceCount: 1,
-            contentFeedback: [
-                {
-                    rating: 1,
-                    comments: 'Test'
-                }
-            ]
+            spine: {
+                ecarUrl: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_21281258639073280011490/sb-13081-2_1564037681733_do_21281258639073280011490_2.0_spine.ecar',
+                size: 136688
+            }
         },
-        status: true
-    }
+        mimeType: 'application/vnd.ekstep.content-collection',
+        leafNodes: [
+            'do_212686708394631168156',
+            'do_212686715674877952160',
+            'do_212608790640934912148',
+            'do_21266836993792409612969',
+            'do_212686673493696512119',
+            'do_2127319848127283201364',
+            'do_21265698034243174413049'
+        ],
+        appIcon: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_21281254676783104011480/artifact/1500_1560755661944.thumb.jpg',
+        gradeLevel: [
+            'Class 2', 'Class 3'
+        ],
+        attributions: ['gd_1', 'gd_2'],
+        me_totalRatings: '4.0',
+        appId: 'staging.diksha.app',
+        contentEncoding: 'gzip',
+        c_Diksha_Stage_open_batch_count: 1,
+        lockKey: '2e55369b-53e3-4e96-8ef1-25bd6da0642a',
+        mimeTypesCount: '{\'application/vnd.ekstep.h5p-archive\:1,\'application/vnd.ekstep.html-archive\:1,\'video/webm\:1,\'application/pdf\:1,\'application/epub\:1,\'application/vnd.ekstep.content-collection\:7,\'video/x-youtube\:1,\'video/mp4\:1}',
+        totalCompressedSize: 9617867,
+        contentCredits: [
+            {
+                id: '0125683555607347207',
+                name: 'Sachin 2808',
+                type: 'user'
+            }
+        ],
+        contentType: 'Course',
+        lastUpdatedBy: 'ab467e6e-1f32-453c-b1d8-c6b5fa6c7b9e',
+        identifier: 'do_21281258639073280011490',
+        audience: [
+            'Learner'
+        ],
+        visibility: 'Default',
+        toc_url: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_21281258639073280011490/artifact/do_21281258639073280011490_toc.json',
+        contentTypesCount: '{\'CourseUnit\:7,\'Resource\:7}',
+        childNodes: [
+            'do_21281258639128166411497',
+            'do_212686715674877952160',
+            'do_212608790640934912148',
+            'do_21281258639126528011492',
+            'do_21281258639126528011493',
+            'do_21266836993792409612969',
+            'do_21281258639127347211494',
+            'do_21281258639127347211495',
+            'do_21281258639127347211496',
+            'do_21265698034243174413049',
+            'do_21281258639125708811491',
+            'do_212686708394631168156',
+            'do_212686673493696512119',
+            'do_2127319848127283201364'
+        ],
+        consumerId: 'a9cb3a83-a164-4bf0-aa49-b834cebf1c07',
+        mediaType: 'content',
+        osId: 'org.ekstep.quiz.app',
+        ageGroup: [
+            '5-6'
+        ],
+        languageCode: [
+            'en',
+            'ka'
+        ],
+        lastPublishedBy: 'dca7518d-5886-4251-94aa-360c762b1182',
+        version: 2,
+        c_diksha_stage_open_batch_count: 1,
+        tags: [
+            'test'
+        ],
+        prevState: 'Review',
+        license: 'Creative Commons Attribution (CC BY)',
+        lastPublishedOn: '2019-07-25T06:54:41.549+0000',
+        size: '136688',
+        domain: [
+            'Artificial_Intelligence'
+        ],
+        name: 'SB-13081-2',
+        topic: [
+            'Teaching and Classroom Management'
+        ],
+        status: '',
+        code: 'org.sunbird.64hCxM.copy',
+        purpose: 'Teaching Techniques',
+        origin: 'do_21281254676783104011480',
+        description: 'Enter description for Course',
+        medium: 'English',
+        idealScreenSize: 'normal',
+        posterImage: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127857103817932801565/artifact/1500_1560755661944.jpg',
+        createdOn: '2019-07 - 25T06: 33: 44.651+0000',
+        c_Diksha_Stage_private_batch_count: 0,
+        contentDisposition: 'inline',
+        lastUpdatedOn: '2019-07 - 25T06: 54: 40.288+0000',
+        originData: {
+            license: 'Creative Commons Attribution(CC BY)',
+            name: 'SB- 13081',
+
+        },
+        SYS_INTERNAL_LAST_UPDATED_ON: '2019 - 08 - 09T18: 33: 01.908 + 0000',
+        dialcodeRequired: 'No',
+        creator: 'Qualitrix Content Creator Cr',
+        createdFor: [
+            '0124784842112040965'
+        ],
+        lastStatusChangedOn: '2019 - 07 - 25T06: 54: 40.275 + 0000',
+        os: [
+            'All'
+        ],
+        pkgVersion: '2',
+        versionKey: '1564037680856',
+        idealScreenDensity: 'hdpi',
+        s3Key: 'ecar_files / do_21281258639073280011490 / sb - 13081 -2_1564037681733_do_21281258639073280011490_2.0_spine.ecar',
+        depth: 0,
+        dialcodes: [
+            'H7L1Q1'
+        ],
+        framework: 'TPD',
+        lastSubmittedOn: '2019 - 07 - 25T06: 53: 44.465 + 0000',
+        createdBy: 'ab467e6e - 1f32 - 453c - b1d8 - c6b5fa6c7b9e',
+        leafNodesCount: 7,
+        compatibilityLevel: 4,
+        resourceType: 'Course',
+        licenseDetails: { description: '', name: '', url: '' },
+        isAvailableLocally: true
+    },
+    isUpdateAvailable: false,
+    mimeType: 'application / vnd.ekstep.content - collection',
+    basePath: '',
+    contentType: 'course',
+    isAvailableLocally: true,
+    referenceCount: 0,
+    sizeOnDevice: 0,
+    lastUsedTime: 0,
+    lastUpdatedTime: 0,
+    contentAccess: [],
+    contentFeedback: [
+        {
+            contentId: 'SAMPLE_ID',
+            rating: 4,
+            comments: 'SAMPLE_COMMANTS',
+            contentVersion: 'SAMPLE_VERSION_6'
+        }, {
+            contentId: 'SAMPLE_ID_1',
+            rating: 4,
+            comments: 'SAMPLE_COMMANTS',
+            contentVersion: 'SAMPLE_VERSION_6'
+        }
+    ],
 };
