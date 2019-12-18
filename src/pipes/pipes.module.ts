9c8a97edf38a1f8d3d6bef1b3f615c5c3960e658
balakrishna@mantralabsglobal.com <m.balakrishna10@gmail.com>
2019-12-18 16:44:01 +0530

    Issue #SB-15905 feat: Removing swimlane

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,11 +1,12 @@
+import { SortByPipe } from './sortby/sortby.pipe';
 import { MimeTypePipe } from './mime-type/mime-type';
 import { NgModule } from '@angular/core';
 import { FileSizePipe } from './file-size/file-size';
 import { CSAPipe } from './csa/csa';
 import { FilterPipe } from './filter/filter.pipe';
 @NgModule({
-  declarations: [FileSizePipe, CSAPipe, MimeTypePipe , FilterPipe],
+  declarations: [FileSizePipe, CSAPipe, MimeTypePipe , FilterPipe, SortByPipe],
   imports: [],
-  exports: [FileSizePipe, CSAPipe, MimeTypePipe , FilterPipe]
+  exports: [FileSizePipe, CSAPipe, MimeTypePipe , FilterPipe, SortByPipe]
 })
 export class PipesModule {}
