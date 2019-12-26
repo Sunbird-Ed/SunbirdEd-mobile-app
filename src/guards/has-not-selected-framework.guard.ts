b0543defd51c9f34c8dddfb9fc1b8c94032c70de
Anil Gupta <ani2824@gmail.com>
2019-12-26 15:51:01 +0530

    Issue #SB-0000 chore: Removed unused import.

--- src/guards/has-not-selected-framework.guard.ts
+++ src/guards/has-not-selected-framework.guard.ts
@@ -1,22 +1,19 @@
 import { Injectable, Inject } from '@angular/core';
-import { CanLoad, Router, ActivatedRoute, Resolve } from '@angular/router';
-import { SharedPreferences, ProfileService } from 'sunbird-sdk';
+import { Router, Resolve } from '@angular/router';
+import { ProfileService } from 'sunbird-sdk';
 import { GenericAppConfig, ProfileConstants } from '@app/app/app.constant';
 import { UtilityService } from '@app/services/utility-service';
 import { AppGlobalService } from '@app/services/app-global-service.service';
-import { CanDeactivate } from '@angular/router';
 import { SplashScreenService } from '@app/services/splash-screen.service';
 
 @Injectable()
 export class HasNotSelectedFrameworkGuard implements Resolve<any> {
     guardActivated: boolean;
     constructor(
-        @Inject('SHARED_PREFERENCES') private sharedPreferences: SharedPreferences,
         @Inject('PROFILE_SERVICE') private profileService: ProfileService,
         private appGlobalService: AppGlobalService,
         private utilityService: UtilityService,
         private router: Router,
-        private activatedRoute: ActivatedRoute,
         private splashScreenService: SplashScreenService
     ) {
     }
@@ -35,15 +32,16 @@ export class HasNotSelectedFrameworkGuard implements Resolve<any> {
         }
         this.guardActivated = true;
         this.utilityService.getBuildConfigValue(GenericAppConfig.DISPLAY_ONBOARDING_CATEGORY_PAGE).then((shouldDisplay) => {
-            this.profileService.getActiveSessionProfile({ requiredFields: ProfileConstants.REQUIRED_FIELDS }).toPromise().then((profile) => {
-                if (shouldDisplay && !HasNotSelectedFrameworkGuard.isProfileComplete(profile)) {
-                    return true;
-                } else {
-                    this.appGlobalService.isProfileSettingsCompleted = true;
-                    this.splashScreenService.handleSunbirdSplashScreenActions();
-                    this.router.navigate(['/', 'tabs']);
-                }
-            });
+            this.profileService.getActiveSessionProfile({ requiredFields: ProfileConstants.REQUIRED_FIELDS }).toPromise()
+                .then((profile) => {
+                    if (shouldDisplay && !HasNotSelectedFrameworkGuard.isProfileComplete(profile)) {
+                        return true;
+                    } else {
+                        this.appGlobalService.isProfileSettingsCompleted = true;
+                        this.splashScreenService.handleSunbirdSplashScreenActions();
+                        this.router.navigate(['/', 'tabs']);
+                    }
+                });
         });
     }
 }
