1478aad64bee6885a73c3fab7fd009e297a87b32
Anil Gupta <ani2824@gmail.com>
2020-01-03 15:19:55 +0530

    Issue #SB-16926 feat: Updated the shared preference key name.

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -191,7 +191,7 @@ export class FormConstant {
 
 export class PreferenceKey {
     public static readonly SELECTED_LANGUAGE_CODE = 'sunbirdselected_language_code';
-    public static readonly DEVICE_LOCATION = 'device_location';
+    public static readonly DEVICE_LOCATION = 'device_location_new';
     public static readonly IP_LOCATION = 'ip_location';
     public static readonly SELECTED_LANGUAGE = 'sunbirdselected_language';
     public static readonly SELECTED_USER_TYPE = 'sunbirdselected_user_type';
