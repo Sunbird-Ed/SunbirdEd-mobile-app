c4d877c26f8d58cdda24766b9906fd60e47bfe70
Anil Gupta <ani2824@gmail.com>
2020-01-28 14:42:20 +0530

    Issue #SB-16767 feat: Display upgrade popup if app is not compatible for any deeplink.

--- src/services/app-global-service.service.ts
+++ src/services/app-global-service.service.ts
@@ -693,9 +693,22 @@ export class AppGlobalService implements OnDestroy {
 
     async closeSigninOnboardingLoader() {
         if (this.signinOnboardingLoader) {
-          await this.signinOnboardingLoader.dismiss();
-          this.signinOnboardingLoader = null;
+            await this.signinOnboardingLoader.dismiss();
+            this.signinOnboardingLoader = null;
         }
-      }
+    }
+
+    getAppVersionCode(): Promise<number> {
+        return new Promise<number>((resolve, reject) => {
+            this.utilityService.getBuildConfigValue(GenericAppConfig.VERSION_CODE)
+                .then(response => {
+                    resolve(+response);
+                })
+                .catch(error => {
+                    console.log('Error--', error);
+                    resolve(0);
+                });
+        });
+    }
 
 }
