211e51ad3233bab05377aa02bf909b41ccbe4392
swayangjit <swayangjit029@gmail.com>
2020-01-29 23:20:23 +0530

    Issue #SB-000 fix: Added telemetry fix for courses.

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -213,6 +213,7 @@ export class PreferenceKey {
     public static readonly DEPLOYMENT_KEY = 'deployment_key';
     public static readonly COURSE_DATA_KEY = 'course_data';
     public static readonly BATCH_DETAIL_KEY = 'batch_detail';
+    public static readonly CDATA_KEY = 'correlation';
     public static readonly IS_LOCATION_MANDATORY = 'is_location_mandatory';
     public static readonly SUBSCRIBE_TOPICS = 'subscribe_topics';
     public static readonly SHOW_EXTERNAL_VERIFICATION = 'show_external_verification';
