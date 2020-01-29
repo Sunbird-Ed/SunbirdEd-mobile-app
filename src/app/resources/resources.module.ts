57cd12ad914555f6b5d409dd4397bfafdc89eb99
christopher.fernandes <christyabcd.05@gmail.com>
2020-01-29 12:38:59 +0530

    Issue #SB-15811 feat: Integrated library page with common consumption library

--- src/app/resources/resources.module.ts
+++ src/app/resources/resources.module.ts
@@ -8,6 +8,7 @@ import { ComponentsModule } from '../components/components.module';
 import { DirectivesModule } from '@app/directives/directives.module';
 import { ExploreBooksSortComponent } from './explore-books-sort/explore-books-sort.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonConsumptionModule } from '@project-sunbird/common-consumption';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ]),
     ComponentsModule,
     DirectivesModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonConsumptionModule
   ],
   exports: [
     ResourcesComponent
