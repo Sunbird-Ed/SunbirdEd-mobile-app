47681057cc1b8fbdd9f030baa663f77d1531ca2e
subranil <subranil.saha@tarento.com>
2020-02-06 12:34:55 +0530

    Issue #SB-17631 fix: force update popup dismiss removed

--- src/app/components/popups/upgrade-popover/upgrade-popover.component.spec.ts
+++ src/app/components/popups/upgrade-popover/upgrade-popover.component.spec.ts
@@ -86,10 +86,12 @@ describe('UpgradePopoverComponent', () => {
 
     it('should invoke openPlayStore', () => {
         // arrange
+        upgradePopoverComponent.upgradeType.type = 'optional';
         // act
         upgradePopoverComponent.upgradeApp('https://play.google.com/store/apps/details?id=org.sunbird.app');
         // assert
         expect(mockUtilityService.openPlayStore).toHaveBeenCalledWith('org.sunbird.app');
+        expect(mockPopOverController.dismiss).toHaveBeenCalled();
     });
 
     it('should generate impression and interact when popoup shows', (done) => {
