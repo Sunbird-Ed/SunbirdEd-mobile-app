74371331f2c70b1c8f56cb8b8c9d7b4337b94261
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2020-04-09 15:03:47 +0530

    Issue #SB-18799 feat: Deeplick auto onboarding for channel details

--- src/services/sunbird-splashscreen/splaschreen-deeplink-action-handler-delegate.ts
+++ src/services/sunbird-splashscreen/splaschreen-deeplink-action-handler-delegate.ts
@@ -1,9 +1,9 @@
-import { ContentFilterConfig, PreferenceKey, ProfileConstants } from '@app/app/app.constant';
+import { ContentFilterConfig, PreferenceKey, ProfileConstants, appLanguages } from '@app/app/app.constant';
 import { Inject, Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { Events, PopoverController } from '@ionic/angular';
 import { Observable, of } from 'rxjs';
-import { PageAssembleService, FrameworkService, ContentService, SharedPreferences, HttpServerError, NetworkError, AuthService, ProfileType, Content, ProfileService } from 'sunbird-sdk';
+import { PageAssembleService, FrameworkService, ContentService, SharedPreferences, HttpServerError, NetworkError, AuthService, ProfileType, Content, ProfileService, Channel, GetFrameworkCategoryTermsRequest, CachedItemRequestSourceFrom, FrameworkCategoryCode, FrameworkUtilService, Profile, FrameworkCategoryCodesGroup, ProfileSource } from 'sunbird-sdk';
 import { SplashscreenActionHandlerDelegate } from './splashscreen-action-handler-delegate';
 import { ContentType, MimeType, EventTopics, RouterLinks, LaunchType } from '../../app/app.constant';
 import { AppGlobalService } from '../app-global-service.service';
@@ -15,9 +15,13 @@ import { UtilityService } from '../utility-service';
 import { SbPopoverComponent } from '@app/app/components/popups/sb-popover/sb-popover.component';
 import { LoginHandlerService } from '../login-handler.service';
 import { TranslateService } from '@ngx-translate/core';
+import { ExternalChannelOverrideListener } from './external-channel-override-interface';
+import { resolve } from 'dns';
+import { initTabs, GUEST_TEACHER_TABS } from '@app/app/module.service';
+import { ContainerService } from '../container.services';
 
 @Injectable()
-export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenActionHandlerDelegate {
+export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenActionHandlerDelegate, ExternalChannelOverrideListener {
   private savedUrlMatch: any;
 
   private _isDelegateReady = false;
@@ -40,6 +44,7 @@ export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenAct
     @Inject('PROFILE_SERVICE') private profileService: ProfileService,
     @Inject('PAGE_ASSEMBLE_SERVICE') private pageAssembleService: PageAssembleService,
     @Inject('FRAMEWORK_SERVICE') private frameworkService: FrameworkService,
+    @Inject('FRAMEWORK_UTIL_SERVICE') private frameworkUtilService: FrameworkUtilService,
     private telemetryGeneratorService: TelemetryGeneratorService,
     private commonUtilService: CommonUtilService,
     private appGlobalServices: AppGlobalService,
@@ -49,7 +54,8 @@ export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenAct
     private utilityService: UtilityService,
     private popoverCtrl: PopoverController,
     private loginHandlerService: LoginHandlerService,
-    public translateService: TranslateService
+    public translateService: TranslateService,
+    private container: ContainerService
   ) {
     this.eventToSetDefaultOnboardingData();
   }
@@ -64,40 +70,65 @@ export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenAct
       const urlRegex = new RegExp(quizTypeRegex.source + '|' + dialTypeRegex.source + '|' +
         contentTypeRegex.source + '|' + courseTypeRegex.source);
       const urlMatch = payload.url.match(urlRegex.source);
-
-      try {
-        const url: URL = new URL(payload.url);
-        const overrideChannelSlug = url.searchParams.get('channel');
-
-        if (overrideChannelSlug) {
-          this.frameworkService.searchOrganization({
-            filters: {
-              slug: overrideChannelSlug,
-              isRootOrg: true
-            } as any
-          }).toPromise().then((result) => {
-            const org: any = result.content && result.content[0];
-
-            if (org) {
-              this.pageAssembleService.setPageAssembleChannel({
-                channelId: org.identifier
-              });
-
-              setTimeout(() => {
-                this.events.publish(EventTopics.COURSE_PAGE_ASSEMBLE_CHANNEL_CHANGE);
-              }, 500);
-            }
-          });
-        }
-      } catch (e) {
-        console.error(e);
-      }
-
-      if (urlMatch && urlMatch.groups) {
-        this.checkIfOnboardingComplete(urlMatch);
-      }
+      this.checkCourseChannelSlug(payload, urlMatch);
     }
     return of(undefined);
+  }
+
+  private async checkCourseChannelSlug(payload, urlMatch) {
+    if (!this.isOnboardingCompleted) {
+      this.isOnboardingCompleted =
+        (await this.preferences.getString(PreferenceKey.IS_ONBOARDING_COMPLETED).toPromise() === 'true') ? true : false;
+    }
+
+    try {
+      const url: URL = new URL(payload.url);
+      const overrideChannelSlug = url.searchParams.get('channel');
+      const isUrlTypeCourse = (new RegExp(String.raw`explore-course`)).test(payload.url);
+
+      if (overrideChannelSlug && isUrlTypeCourse) {
+        const filters = {
+          slug: overrideChannelSlug,
+          isRootOrg: true
+        };
+        const result = await this.frameworkService.searchOrganization({ filters }).toPromise();
+        const org: any = result.content && result.content[0];
+        if (org) {
+          this.pageAssembleService.setPageAssembleChannel({
+            channelId: org.identifier
+          });
+
+          setTimeout(() => {
+            this.events.publish(EventTopics.COURSE_PAGE_ASSEMBLE_CHANNEL_CHANGE);
+          }, 500);
+          let courseId;
+          if (urlMatch && urlMatch.groups && urlMatch.groups.courseId) {
+            courseId = urlMatch.groups.courseId;
+          }
+          const event = {
+            url: payload.url,
+            courseId,
+            avb: '',
+            channelId: org.identifier,
+            extras: {
+              profile: {
+                userType: url.searchParams.get('role'),
+                langCode: url.searchParams.get('lang')
+              }
+            }
+          };
+          if (await this.onChannelDetected(event)) {
+            return;
+          }
+        }
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    if (urlMatch && urlMatch.groups) {
+      this.checkIfOnboardingComplete(urlMatch);
+    }
   }
 
   private async checkIfOnboardingComplete(urlMatch) {
@@ -322,21 +353,143 @@ export class SplaschreenDeeplinkActionHandlerDelegate implements SplashscreenAct
   }
 
   private async setDefaultLanguageAndUserType(params?) {
+    let selectedLanguage;
+    if (params && params.langCode) {
+      const LangList = appLanguages;
+      selectedLanguage = LangList.find(i => i.code === params.langCode);
+    }
+
     const langCode = await this.preferences.getString(PreferenceKey.SELECTED_LANGUAGE_CODE).toPromise();
     if (!langCode) {
-      await this.preferences.putString(PreferenceKey.SELECTED_LANGUAGE_CODE, (params && params.langCode) || 'en').toPromise();
+      await this.preferences.putString(PreferenceKey.SELECTED_LANGUAGE_CODE, (selectedLanguage && selectedLanguage.code) || 'en').toPromise();
       this.translateService.use('en');
     }
 
     const langLabel = await this.preferences.getString(PreferenceKey.SELECTED_LANGUAGE).toPromise();
     if (!langLabel) {
-      await this.preferences.putString(PreferenceKey.SELECTED_LANGUAGE, (params && params.langName) || 'English').toPromise();
+      await this.preferences.putString(PreferenceKey.SELECTED_LANGUAGE, (selectedLanguage && selectedLanguage.name) || 'English').toPromise();
     }
 
     // usertyoe == "TEACHER" for Quiz-link | "OTHER" for course link
     const userType = await this.preferences.getString(PreferenceKey.SELECTED_USER_TYPE).toPromise();
     if (!userType) {
       await this.preferences.putString(PreferenceKey.SELECTED_USER_TYPE, (params && params.userType) || ProfileType.TEACHER).toPromise();
+    }
+  }
+
+  async onChannelDetected(event): Promise<boolean> {
+    if (!this.isOnboardingCompleted) {
+      this.isOnboardingCompleted =
+        (await this.preferences.getString(PreferenceKey.IS_ONBOARDING_COMPLETED).toPromise() === 'true') ? true : false;
+    }
+    if (this.isOnboardingCompleted) {
+      this.navigateToCourse(event.courseId);
+      return true;
+    }
+
+    const params = {
+      userType: (Object.values(ProfileType).includes(event.extras.profile.userType)) ? event.extras.profile.userType : undefined,
+      langCode: event.extras.profile.langCode || undefined
+    };
+    this.setDefaultLanguageAndUserType(params);
+
+    if (await this.setCourseOnboardingFlow(event)) {
+      this.appGlobalServices.skipCoachScreenForDeeplink = true;
+      return true;
+    }
+    return false;
+  }
+
+  async navigateToCourse(courseId) {
+    if (courseId) {
+      const content: any = await this.getContentData(courseId);
+      if (content && content.contentType === ContentType.COURSE.toLowerCase()) {
+        this.router.navigate([RouterLinks.ENROLLED_COURSE_DETAILS], { state: { content, isFromChannelDeeplink: true } });
+      }
+    } else {
+      this.router.navigateByUrl(RouterLinks.TABS_COURSE);
+    }
+  }
+
+  setCourseOnboardingFlow(event) {
+
+    return new Promise(async resolve => {
+      try {
+        const channelDetails: Channel = await this.frameworkService.getChannelDetails({ channelId: event.channelId }).toPromise();
+        const frameworkId = channelDetails.defaultFramework;
+        const syllabus = frameworkId;
+
+        const categories = [
+          { code: FrameworkCategoryCode.BOARD, prevCode: null },
+          { code: FrameworkCategoryCode.MEDIUM, prevCode: FrameworkCategoryCode.BOARD },
+          { code: FrameworkCategoryCode.GRADE_LEVEL, prevCode: FrameworkCategoryCode.MEDIUM }
+        ];
+        const categoryData: any = {};
+        for (const category of categories) {
+          const boardCategoryTermsRequet: GetFrameworkCategoryTermsRequest = {
+            from: category.code === FrameworkCategoryCode.BOARD ? CachedItemRequestSourceFrom.SERVER : null,
+            frameworkId,
+            requiredCategories: FrameworkCategoryCodesGroup.DEFAULT_FRAMEWORK_CATEGORIES,
+            currentCategoryCode: category.code,
+            language: this.translateService.currentLang,
+            selectedTermsCodes: category.prevCode ? [category.prevCode] : null
+          };
+          const terms = (await this.frameworkUtilService.getFrameworkCategoryTerms(boardCategoryTermsRequet).toPromise());
+          categoryData[category.code] = terms[0].code;
+        }
+
+        const payload = {
+          syllabus,
+          board: categoryData[FrameworkCategoryCode.BOARD],
+          medium: categoryData[FrameworkCategoryCode.MEDIUM],
+          grade: categoryData[FrameworkCategoryCode.GRADE_LEVEL],
+          courseId: event.courseId
+        };
+        this.submitProfileSettings(payload);
+        resolve(true);
+      } catch (e) {
+        resolve(false);
+        console.log(e);
+      }
+    });
+  }
+
+  private async submitProfileSettings(payload) {
+    try {
+      const activeSessionProfile = await this.profileService.getActiveSessionProfile({
+        requiredFields: ProfileConstants.REQUIRED_FIELDS
+      }).toPromise();
+      const userType = await this.preferences.getString(PreferenceKey.SELECTED_USER_TYPE).toPromise();
+
+      const updateProfileRequest: Profile = {
+        ...activeSessionProfile,
+        syllabus: [payload.syllabus],
+        board: [payload.board],
+        medium: [payload.medium],
+        grade: [payload.grade],
+        handle: 'Guest1',
+        profileType: userType as any,
+        source: ProfileSource.LOCAL
+      };
+
+      const profile: Profile = await this.profileService.updateProfile(updateProfileRequest).toPromise();
+      initTabs(this.container, GUEST_TEACHER_TABS);
+      this.events.publish('refresh:profile');
+      this.appGlobalServices.guestUserProfile = profile;
+
+      this.commonUtilService.handleToTopicBasedNotification();
+
+      setTimeout(async () => {
+        this.appGlobalServices.setOnBoardingCompleted();
+        this.navigateToCourse(payload.courseId);
+      }, 1000);
+
+      this.events.publish('onboarding-card:completed', { isOnBoardingCardCompleted: true });
+      this.events.publish('refresh:profile');
+      this.appGlobalServices.guestUserProfile = profile;
+
+    } catch (e) {
+      console.log(e);
     }
   }
 
