ce3cdb42d092cad84b707c2152d12397dcc935e4
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2020-04-24 09:29:11 +0530

    Issue #SB-0000 fix: removing space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -170,7 +170,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         corRelationList
       );
     }
-
     this.notificationSrc.setupLocalNotification();
 
     this.triggerSignInEvent();
