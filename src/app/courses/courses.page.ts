35d32e94623ebe7cbf00233a464b355db95224a4
Ajoymaity <ajoymaity17@gmail.com>
2020-05-12 18:27:12 +0530

    Issue #SB-19285 fix: Fixed course progress problem for enrolled corses

--- src/app/courses/courses.page.ts
+++ src/app/courses/courses.page.ts
@@ -302,6 +302,9 @@ export class CoursesPage implements OnInit, OnDestroy {
               for (let count = 0; count < this.enrolledCourses.length; count++){
                 courseList.push(this.enrolledCourses[count]);
                 this.enrolledCourses[count]['cardImg'] = this.commonUtilService.getContentImg(this.enrolledCourses[count]);
+                if (this.enrolledCourses[count]['completionPercentage'] === undefined) {
+                  this.enrolledCourses[count]['completionPercentage'] = 0;
+                }
               }
 
               this.appGlobalService.setEnrolledCourseList(courseList);
