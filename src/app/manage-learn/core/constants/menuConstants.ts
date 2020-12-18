26a0166580b33af8337ad4eb6169dfc045c35a96
harpreetwalia143@gmail.com <harpreetwalia143@gmail.com>
2020-12-18 15:38:43 +0530

    feat: #21826 Project Details header icons and design popup done

---
+++ src/app/manage-learn/core/constants/menuConstants.ts
@@ -0,0 +1,28 @@
+export const menuConstants = {
+    TASK: [{
+        TITLE: 'SHARE_TASK',
+        VALUE: 'shareTask',
+        ICON: 'share'
+    },{
+        TITLE: 'EDIT',
+        VALUE: 'editTask',
+        ICON: 'create'
+    }],
+    PROJECT: [{
+        TITLE: 'SHARE_PROJECT',
+        VALUE: 'shareProject',
+        ICON: 'share'
+    },{
+        TITLE: 'EDIT',
+        VALUE: 'editProject',
+        ICON: 'create'
+    },{
+        TITLE: 'DELETE',
+        VALUE: 'deleteProject',
+        ICON: 'trash'
+    },{
+        TITLE: 'LABELS_FILES',
+        VALUE: 'fileProject',
+        ICON: 'document'
+    }],
+};
--- src/app/manage-learn/shared/shared.module.ts
+++ src/app/manage-learn/shared/shared.module.ts
@@ -2,23 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CamelToTitlePipe } from './pipe/camel-to-title.pipe';
 import { UtilsService } from '../core/services/utils.service';
-import { CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CommonListCardComponent } from './components';
 import { ItemListCardComponent } from './components/item-list-card/item-list-card.component';
 import { CommonHeaderComponent } from './components/common-header/common-header.component';
+import { PopoverComponent } from './components/popover/popover.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  
-  declarations: [CamelToTitlePipe, CommonListCardComponent, ItemListCardComponent, CommonHeaderComponent],
+
+  declarations: [CamelToTitlePipe, CommonListCardComponent, ItemListCardComponent, CommonHeaderComponent,
+    PopoverComponent],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    TranslateModule.forChild(),
     ReactiveFormsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  exports: [CamelToTitlePipe, CommonListCardComponent, ItemListCardComponent, CommonHeaderComponent ]
+  exports: [CamelToTitlePipe, CommonListCardComponent, ItemListCardComponent, CommonHeaderComponent,
+    PopoverComponent],
+    entryComponents: [PopoverComponent]
 })
 export class SharedModule { }
