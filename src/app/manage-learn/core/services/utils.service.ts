0015e7db9890d3cb8da0db0994c20ee44c94d141
rejneesh1 <pillairejneesh@gmail.com>
2020-12-23 15:34:03 +0530

    SB-21831 fix-Section and question page connected

--- src/app/manage-learn/core/services/utils.service.ts
+++ src/app/manage-learn/core/services/utils.service.ts
@@ -6,18 +6,19 @@ import { statusType } from '@app/app/manage-learn/core/constants/statuses.consta
 
 @Injectable()
 export class UtilsService {
-  constructor() { }
+  imagePath: string;
+  constructor() {}
 
   generateFileName(name: string[] = []) {
     const d = new Date();
-    const fullTime = moment(d).format("DD-MM-YYYY-hh-mm-ss");
+    const fullTime = moment(d).format('DD-MM-YYYY-hh-mm-ss');
 
     name.push(fullTime);
-    return name.join("_");
+    return name.join('_');
   }
 
   cameltoNormalCase(word) {
-    return word.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return word.replace(/([A-Z])/g, ' $1').replace(/^./, function (str) {
       return str.toUpperCase();
     });
   }
@@ -39,8 +40,8 @@ export class UtilsService {
    Output: website=unnati&location=india
    */
   encodeQuery(data) {
-    let query = "";
-    for (let d in data) query += encodeURIComponent(d) + "=" + encodeURIComponent(data[d]) + "&";
+    let query = '';
+    for (let d in data) query += encodeURIComponent(d) + '=' + encodeURIComponent(data[d]) + '&';
     return query.slice(0, -1);
   }
 
@@ -48,12 +49,12 @@ export class UtilsService {
     const obj = {
       _id: uuidv4(),
       status: statusType.notStarted,
-      name: "",
-      endDate: "",
-      assignee: "",
-      type: "simple",
+      name: '',
+      endDate: '',
+      assignee: '',
+      type: 'simple',
       attachments: [],
-      startDate: "",
+      startDate: '',
       isNew: true,
       isEdit: true,
       children: [],
@@ -61,9 +62,9 @@ export class UtilsService {
       isDeletable: true,
     };
     switch (type) {
-      case "task":
+      case 'task':
         return obj;
-      case "subTask":
+      case 'subTask':
         delete obj.children;
         delete obj.isDeletable;
         return obj;
@@ -72,27 +73,27 @@ export class UtilsService {
   getTaskSortMeta() {
     const data = {
       past: {
-        label: "LABELS.PAST",
+        label: 'LABELS.PAST',
         tasks: [],
       },
       today: {
-        label: "LABELS.TODAY",
+        label: 'LABELS.TODAY',
         tasks: [],
       },
       thisWeek: {
-        label: "LABELS.THIS_WEEK",
+        label: 'LABELS.THIS_WEEK',
         tasks: [],
       },
       thisMonth: {
-        label: "LABELS.THIS_MONTH",
+        label: 'LABELS.THIS_MONTH',
         tasks: [],
       },
       thisQuarter: {
-        label: "LABELS.THIS_QUARTER",
+        label: 'LABELS.THIS_QUARTER',
         tasks: [],
       },
       upcoming: {
-        label: "LABELS.UPCOMING",
+        label: 'LABELS.UPCOMING',
         tasks: [],
       },
     };
@@ -105,18 +106,16 @@ export class UtilsService {
       task.status = task.children.length ? this.calculateStatus(task.children) : task.status;
 
       // added for assessment or observation submission statuses
-      if (task.type == "assessment" || task.type == "observation") {
+      if (task.type == 'assessment' || task.type == 'observation') {
         if (task.submissionDetails && task.submissionDetails.status !== statusType.completed) {
           if (task.status == statusType.completed) {
             task.status = statusType.inProgress;
           }
-
         }
         if (!task.submissionDetails) {
           if (task.status == statusType.completed) {
             task.status = statusType.inProgress;
           }
-
         }
 
         if (task.submissionDetails && task.submissionDetails.status == statusType.completed && !task.children.length) {
@@ -159,7 +158,7 @@ export class UtilsService {
     } else {
       status = statusType.notStarted;
     }
-    return validchildArray.length ? status : statusType.notStarted
+    return validchildArray.length ? status : statusType.notStarted;
   }
 
   checkForTaskCompletion(task) {
@@ -204,17 +203,20 @@ export class UtilsService {
 
   getFileExtensions(url) {
     let splittedString = url.split('.');
-    let splittedStringForName = url.split('/')
+    let splittedStringForName = url.split('/');
     const obj = {
       type: splittedString[splittedString.length - 1],
-      name: splittedStringForName[splittedStringForName.length - 1]
-    }
-    return obj
+      name: splittedStringForName[splittedStringForName.length - 1],
+    };
+    return obj;
   }
 
   getAssessmentLocalStorageKey(entityId) {
-    // return this.currentAssessmentType ? this.currentAssessmentType + schoolId : "schoolDetails_" + schoolId
-    return 'assessmentDetails_' + entityId
+    
+    return 'assessmentDetails_' + entityId;
+  }
+  setCurrentimageFolderName(evidenceId, schoolId) {
+    this.imagePath = 'images_' + evidenceId + '_' + schoolId;
   }
 
   checkForDependentVisibility(qst, allQuestion): boolean {
@@ -223,16 +225,16 @@ export class UtilsService {
       for (const condition of qst.visibleIf) {
         if (condition._id === question._id) {
           let expression = [];
-          if (condition.operator != "===") {
+          if (condition.operator != '===') {
             if (question.responseType === 'multiselect') {
               for (const parentValue of question.value) {
                 for (const value of condition.value) {
-                  expression.push("(", "'" + parentValue + "'", "===", "'" + value + "'", ")", condition.operator);
+                  expression.push('(', "'" + parentValue + "'", '===', "'" + value + "'", ')', condition.operator);
                 }
               }
             } else {
               for (const value of condition.value) {
-                expression.push("(", "'" + question.value + "'", "===", "'" + value + "'", ")", condition.operator)
+                expression.push('(', "'" + question.value + "'", '===', "'" + value + "'", ')', condition.operator);
               }
             }
 
@@ -240,52 +242,61 @@ export class UtilsService {
           } else {
             if (question.responseType === 'multiselect') {
               for (const value of question.value) {
-                expression.push("(", "'" + condition.value + "'", "===", "'" + value + "'", ")", "||");
+                expression.push('(', "'" + condition.value + "'", '===', "'" + value + "'", ')', '||');
               }
               expression.pop();
             } else {
-              expression.push("(", "'" + question.value + "'", condition.operator, "'" + condition.value + "'", ")")
+              expression.push('(', "'" + question.value + "'", condition.operator, "'" + condition.value + "'", ')');
             }
           }
           if (!eval(expression.join(''))) {
-            return false
+            return false;
           }
         }
       }
     }
-    return display
+    return display;
   }
 
   isQuestionComplete(question): boolean {
-    if (question.validation.required && question.value === "" && question.responseType !== 'multiselect') {
-      return false
+    if (question.validation.required && question.value === '' && question.responseType !== 'multiselect') {
+      return false;
     }
-    if (question.validation.required && question.value && !question.value.length && question.responseType === 'multiselect') {
-      return false
+    if (
+      question.validation.required &&
+      question.value &&
+      !question.value.length &&
+      question.responseType === 'multiselect'
+    ) {
+      return false;
     }
-    if (question.validation.regex && (question.responseType === 'number' || question.responseType === 'text') && !this.testRegex(question.validation.regex, question.value)) {
-      return false
+    if (
+      question.validation.regex &&
+      (question.responseType === 'number' || question.responseType === 'text') &&
+      !this.testRegex(question.validation.regex, question.value)
+    ) {
+      return false;
     }
-    return true
+    return true;
   }
 
   isMatrixQuestionComplete(question): boolean {
     if (!question.value.length) {
-      return false
+      return false;
     }
     for (const instance of question.value) {
       for (const question of instance) {
         if (!question.isCompleted) {
-          return false
+          return false;
         }
       }
     }
-    return true
+    return true;
   }
   isPageQuestionComplete(question) {
     for (const element of question.pageQuestions) {
       if (!element.isCompleted) {
-        return false
+        return false;
       }
     }
     return true;
@@ -293,7 +304,7 @@ export class UtilsService {
 
   testRegex(rege, value): boolean {
     const regex = new RegExp(rege);
-    return regex.test(value)
+    return regex.test(value);
   }
 
   getCompletedQuestionsCount(questions) {
@@ -303,10 +314,160 @@ export class UtilsService {
         count++;
       }
     }
-    return count
+    return count;
   }
 
   getProjectData() {
-    return {"userId":"01c04166-a65e-4e92-a87b-a9e4194e771d","status":"notStarted","isDeleted":false,"categories":[{"value":"5fc48155b9335656a106c06a","label":"Infrastructure"}],"tasks":[{"_id":"668fd6e2-9c06-458d-a286-6d4f642d3605","createdBy":"f449823a-06bb-4a3f-9d49-edbe1524ebbb","updatedBy":"01c04166-a65e-4e92-a87b-a9e4194e771d","isDeleted":false,"isDeleteable":false,"taskSequence":[],"children":[{"_id":"e08afef0-3857-42d5-9850-8ee882bf8631","status":"notStarted","name":"te","endDate":"","assignee":"","type":"simple","attachments":[],"startDate":"","isDeleted":false,"externalId":"tesstostyurssdtyureany ","isDeleteable":false,"createdAt":"2020-12-01T15:29:07.334Z","updatedAt":"2020-12-01T17:20:04.953Z","isImportedFromLibrary":false,"lastSync":"2020-12-01T17:20:04.953Z","children":[]}],"visibleIf":[],"hasSubTasks":false,"learningResources":[],"deleted":false,"type":"assessment","solutionDetails":{"type":"assessment","subType":"institutional","_id":"5d0a0cf11e724f059a0d8f10","isReusable":true,"externalId":"EF-DCPCR-2018-001-TEMPLATE","name":"DCPCR Assessment Framework 2018"},"projectTemplateId":"5fc4f056ed1ae1783770692f","name":"REJNEESH ASSESSMENT1","externalId":"REJNEESH-ASSESSMENT1","description":"Task-1 Description","updatedAt":"2020-12-01T17:20:04.953Z","createdAt":"2020-11-30T13:23:33.880Z","__v":0,"status":"notStarted","isImportedFromLibrary":false,"lastSync":"2020-12-01T17:20:04.953Z"},{"_id":"67fb7c47-44e2-437f-97f2-e367ec9c4ea8","createdBy":"f449823a-06bb-4a3f-9d49-edbe1524ebbb","updatedBy":"f449823a-06bb-4a3f-9d49-edbe1524ebbb","isDeleted":false,"isDeleteable":false,"taskSequence":[],"children":[],"visibleIf":[],"hasSubTasks":false,"learningResources":[],"deleted":false,"type":"assessment","solutionDetails":{"type":"assessment","subType":"institutional","_id":"5b98fa069f664f7e1ae7498c","isReusable":false,"externalId":"EF-DCPCR-2018-001","name":"DCPCR Assessment Framework 2018"},"projectTemplateId":"5fc4f056ed1ae1783770692f","name":"REJNEESH ASSESSMENT2","externalId":"REJNEESH-ASSESSMENT2","description":"Task-3 Description","updatedAt":"2020-11-30T14:18:46.647Z","createdAt":"2020-11-30T13:23:33.886Z","__v":0,"status":"notStarted","isImportedFromLibrary":true,"lastSync":"2020-11-30T14:18:46.647Z","submissionDetails":{"entityId":"5beaa888af0065f0e0a10515","programId":"5fc4ad7a4f96e8623deacda9","solutionId":"5b98fa069f664f7e1ae7498c"}},{"_id":"4f61c97a-571c-4dcc-b58e-1872230940dc","createdBy":"f449823a-06bb-4a3f-9d49-edbe1524ebbb","updatedBy":"f449823a-06bb-4a3f-9d49-edbe1524ebbb","isDeleted":false,"isDeleteable":false,"taskSequence":[],"children":[],"visibleIf":[],"hasSubTasks":false,"learningResources":[],"deleted":false,"type":"observation","solutionDetails":{"type":"observation","subType":"school","_id":"5d0a0cf11e724f059a0d8f11","isReusable":false,"externalId":"CRO-2019-TEMPLATE","name":"CRO-2019"},"projectTemplateId":"5fc4f056ed1ae1783770692f","name":"REJNEESH OBSERVATION2","externalId":"REJNEESH-OBSERVATION2","description":"Task-2 Description","updatedAt":"2020-11-30T14:18:46.647Z","createdAt":"2020-11-30T13:23:33.890Z","__v":0,"status":"notStarted","isImportedFromLibrary":true,"lastSync":"2020-11-30T14:18:46.647Z"}],"learningResources":[{"name":"Copy Feature","link":"https://dev.bodh.shikshalokam.org/resources/play/content/do_113059727462957056137","app":"bodh","id":"do_113059727462957056137"}],"deleted":false,"title":"REJNEESH-TEST","description":"improving community library","updatedAt":"2020-12-01T17:20:04.954Z","createdAt":"2020-11-30T13:15:02.824Z","lastDownloadedAt":"2020-12-11T04:10:37.799Z","lastSync":"2020-12-01T17:20:04.953Z","entityId":"5beaa888af0065f0e0a10515","entityName":"Apple School","programId":"5fc4ad7a4f96e8623deacda9","programName":"Project -30-nov","rationale":"","primaryAudience":["teachers","head master"],"_id":"5fc4ff46ed1ae17837706937","_rev":"1-28270c873915239807bd35fde4d4157f"};
+    return {
+      userId: '01c04166-a65e-4e92-a87b-a9e4194e771d',
+      status: 'notStarted',
+      isDeleted: false,
+      categories: [{ value: '5fc48155b9335656a106c06a', label: 'Infrastructure' }],
+      tasks: [
+        {
+          _id: '668fd6e2-9c06-458d-a286-6d4f642d3605',
+          createdBy: 'f449823a-06bb-4a3f-9d49-edbe1524ebbb',
+          updatedBy: '01c04166-a65e-4e92-a87b-a9e4194e771d',
+          isDeleted: false,
+          isDeleteable: false,
+          taskSequence: [],
+          children: [
+            {
+              _id: 'e08afef0-3857-42d5-9850-8ee882bf8631',
+              status: 'notStarted',
+              name: 'te',
+              endDate: '',
+              assignee: '',
+              type: 'simple',
+              attachments: [],
+              startDate: '',
+              isDeleted: false,
+              externalId: 'tesstostyurssdtyureany ',
+              isDeleteable: false,
+              createdAt: '2020-12-01T15:29:07.334Z',
+              updatedAt: '2020-12-01T17:20:04.953Z',
+              isImportedFromLibrary: false,
+              lastSync: '2020-12-01T17:20:04.953Z',
+              children: [],
+            },
+          ],
+          visibleIf: [],
+          hasSubTasks: false,
+          learningResources: [],
+          deleted: false,
+          type: 'assessment',
+          solutionDetails: {
+            type: 'assessment',
+            subType: 'institutional',
+            _id: '5d0a0cf11e724f059a0d8f10',
+            isReusable: true,
+            externalId: 'EF-DCPCR-2018-001-TEMPLATE',
+            name: 'DCPCR Assessment Framework 2018',
+          },
+          projectTemplateId: '5fc4f056ed1ae1783770692f',
+          name: 'REJNEESH ASSESSMENT1',
+          externalId: 'REJNEESH-ASSESSMENT1',
+          description: 'Task-1 Description',
+          updatedAt: '2020-12-01T17:20:04.953Z',
+          createdAt: '2020-11-30T13:23:33.880Z',
+          __v: 0,
+          status: 'notStarted',
+          isImportedFromLibrary: false,
+          lastSync: '2020-12-01T17:20:04.953Z',
+        },
+        {
+          _id: '67fb7c47-44e2-437f-97f2-e367ec9c4ea8',
+          createdBy: 'f449823a-06bb-4a3f-9d49-edbe1524ebbb',
+          updatedBy: 'f449823a-06bb-4a3f-9d49-edbe1524ebbb',
+          isDeleted: false,
+          isDeleteable: false,
+          taskSequence: [],
+          children: [],
+          visibleIf: [],
+          hasSubTasks: false,
+          learningResources: [],
+          deleted: false,
+          type: 'assessment',
+          solutionDetails: {
+            type: 'assessment',
+            subType: 'institutional',
+            _id: '5b98fa069f664f7e1ae7498c',
+            isReusable: false,
+            externalId: 'EF-DCPCR-2018-001',
+            name: 'DCPCR Assessment Framework 2018',
+          },
+          projectTemplateId: '5fc4f056ed1ae1783770692f',
+          name: 'REJNEESH ASSESSMENT2',
+          externalId: 'REJNEESH-ASSESSMENT2',
+          description: 'Task-3 Description',
+          updatedAt: '2020-11-30T14:18:46.647Z',
+          createdAt: '2020-11-30T13:23:33.886Z',
+          __v: 0,
+          status: 'notStarted',
+          isImportedFromLibrary: true,
+          lastSync: '2020-11-30T14:18:46.647Z',
+          submissionDetails: {
+            entityId: '5beaa888af0065f0e0a10515',
+            programId: '5fc4ad7a4f96e8623deacda9',
+            solutionId: '5b98fa069f664f7e1ae7498c',
+          },
+        },
+        {
+          _id: '4f61c97a-571c-4dcc-b58e-1872230940dc',
+          createdBy: 'f449823a-06bb-4a3f-9d49-edbe1524ebbb',
+          updatedBy: 'f449823a-06bb-4a3f-9d49-edbe1524ebbb',
+          isDeleted: false,
+          isDeleteable: false,
+          taskSequence: [],
+          children: [],
+          visibleIf: [],
+          hasSubTasks: false,
+          learningResources: [],
+          deleted: false,
+          type: 'observation',
+          solutionDetails: {
+            type: 'observation',
+            subType: 'school',
+            _id: '5d0a0cf11e724f059a0d8f11',
+            isReusable: false,
+            externalId: 'CRO-2019-TEMPLATE',
+            name: 'CRO-2019',
+          },
+          projectTemplateId: '5fc4f056ed1ae1783770692f',
+          name: 'REJNEESH OBSERVATION2',
+          externalId: 'REJNEESH-OBSERVATION2',
+          description: 'Task-2 Description',
+          updatedAt: '2020-11-30T14:18:46.647Z',
+          createdAt: '2020-11-30T13:23:33.890Z',
+          __v: 0,
+          status: 'notStarted',
+          isImportedFromLibrary: true,
+          lastSync: '2020-11-30T14:18:46.647Z',
+        },
+      ],
+      learningResources: [
+        {
+          name: 'Copy Feature',
+          link: 'https://dev.bodh.shikshalokam.org/resources/play/content/do_113059727462957056137',
+          app: 'bodh',
+          id: 'do_113059727462957056137',
+        },
+      ],
+      deleted: false,
+      title: 'REJNEESH-TEST',
+      description: 'improving community library',
+      updatedAt: '2020-12-01T17:20:04.954Z',
+      createdAt: '2020-11-30T13:15:02.824Z',
+      lastDownloadedAt: '2020-12-11T04:10:37.799Z',
+      lastSync: '2020-12-01T17:20:04.953Z',
+      entityId: '5beaa888af0065f0e0a10515',
+      entityName: 'Apple School',
+      programId: '5fc4ad7a4f96e8623deacda9',
+      programName: 'Project -30-nov',
+      rationale: '',
+      primaryAudience: ['teachers', 'head master'],
+      _id: '5fc4ff46ed1ae17837706937',
+      _rev: '1-28270c873915239807bd35fde4d4157f',
+    };
   }
 }
