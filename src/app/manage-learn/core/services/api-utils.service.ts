92c79338a6871e531f144e47f0564808713bae71
kiranTunerlabs <kiran@tunerlabs.com>
2021-02-01 20:05:23 +0530

    SB-21833 : Fix: Comment fixes

--- src/app/manage-learn/core/services/api-utils.service.ts
+++ src/app/manage-learn/core/services/api-utils.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { GenericAppConfig } from '@app/app/app.constant';
-import { UtilityService } from '@app/services';
+import { CommonUtilService, UtilityService } from '@app/services';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +9,19 @@ export class ApiUtilsService {
   public assessmentBaseUrl: string;
   public projectsBaseUrl: string;
   public appVersion;
-  public appName = 'diksha';
+  public appName;
 
   constructor(
-    private utility: UtilityService
+    private utility: UtilityService,
+    private commonUtilService: CommonUtilService,
+
   ) { }
 
   async initilizeML() {
     this.assessmentBaseUrl = !this.assessmentBaseUrl ? await this.utility.getBuildConfigValue("SURVEY_BASE_URL") : this.assessmentBaseUrl;
     this.projectsBaseUrl = !this.projectsBaseUrl ? await this.utility.getBuildConfigValue('PROJECTS_BASE_URL') : this.projectsBaseUrl;
-    this.appVersion = await this.utility.getBuildConfigValue(GenericAppConfig.VERSION_CODE) ;
-
+    this.appVersion = !this.appVersion ? await this.utility.getBuildConfigValue(GenericAppConfig.VERSION_CODE) : this.appVersion;
+    this.appName = !this.appName ? this.commonUtilService.getAppName() : this.appName
   }
 
   getBaseUrl(key) {
