063d6812078c3b96f75e3468ba730104c990fc9c
vishwanathbadiger <vishwanathbadiger@vishwanaths-MacBook-Air.local>
2021-02-03 10:27:49 +0530

    SB-21543  fix : Reports charts changes.

--- src/app/manage-learn/shared/components/chart/chart.component.ts
+++ src/app/manage-learn/shared/components/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef, Input } from '@angular/core';
 import { Chart } from 'chart.js';
 @Component({
   selector: 'app-chart',
@@ -6,37 +6,26 @@ import { Chart } from 'chart.js';
   styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent implements OnInit {
+  @Input() data;
+  // @ViewChild('doughnutCanvas', {static: false}) private doughnutCanvas: ElementRef;
   @ViewChild('doughnutCanvas', {static: false}) private doughnutCanvas: ElementRef;
-
-  private doughnutChart: Chart;
+  private doughnutChart: any;
   constructor() { 
   }
 
   ngOnInit() {
-   this.setup();
+  
+  }
+  ngAfterViewInit() {
+    this.setup();
   }
   setup(){
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+    this.doughnutChart =new Chart(this.doughnutCanvas.nativeElement, {
       type: "doughnut",
       data: {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
-          }
-        ]
+        datasets:  this.data.series
       }
     });
   }
-
 }
