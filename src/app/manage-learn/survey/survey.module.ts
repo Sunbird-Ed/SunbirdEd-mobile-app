099ede5fbf37c3db979d0fb25e9db7d687a96d62
rejneesh1 <pillairejneesh@gmail.com>
2021-02-04 18:31:25 +0530

    SB-21831:Fix:Removed surveyProvider from core module provider

--- src/app/manage-learn/survey/survey.module.ts
+++ src/app/manage-learn/survey/survey.module.ts
@@ -6,6 +6,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
+import { SurveyMsgComponent } from '../shared/components/survey-msg/survey-msg.component';
 const routes: Routes = [
   {
     path: '',
@@ -24,6 +25,7 @@ const routes: Routes = [
     SharedModule,
     CoreModule,
   ],
-  providers:[]
+  providers: [],
+  entryComponents:[SurveyMsgComponent]
 })
 export class SurveyModule {}
