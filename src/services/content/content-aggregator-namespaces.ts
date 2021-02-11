50bedd206f378225c1f02321ccee96cc8767302c
JSDevL <dev.arfath@gmail.com>
2021-02-11 12:31:47 +0530

    Issue #SB-21450 chore: CHANGED: subtype versions for 'Aggregator' form configurations of 'home' and 'discover' pages

--- src/services/content/content-aggregator-namespaces.ts
+++ src/services/content/content-aggregator-namespaces.ts
@@ -1,8 +1,8 @@
 export enum AggregatorPageType {
     COURSE = 'course_v2',
     LIBRARY = 'library_v3',
-    HOME = 'home',
-    DISCOVER = 'discover',
+    HOME = 'home_v2',
+    DISCOVER = 'discover_v2',
     SEARCH = 'search',
     ADMIN_HOME = 'adminHome'
 }
