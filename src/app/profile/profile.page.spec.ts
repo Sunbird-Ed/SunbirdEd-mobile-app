ee11a223557330fece56e80268140343d68b9aaa
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2021-03-05 15:56:06 +0530

    Issue #SB-22856 feat: accessing viewed certificates in offline mode

--- src/app/profile/profile.page.spec.ts
+++ src/app/profile/profile.page.spec.ts
@@ -642,7 +642,10 @@ describe('Profile.page', () => {
                     id: 'sample_cert_id',
                     url: 'https://sampleCertUrl.com',
                     token: 'AXOBC'
-                }
+                },
+                courseName: 'sample_course',
+                dateTime: '1333065600000',
+                status: 0
             });
             // assert
             setTimeout(() => {
@@ -701,7 +704,10 @@ describe('Profile.page', () => {
                     id: 'sample_cert_id',
                     url: 'https://sampleCertUrl.com',
                     token: 'AXOBC'
-                }
+                },
+                courseName: 'sample_course',
+                dateTime: '1333065600000',
+                status: 0
             });
             // assert
             setTimeout(() => {
@@ -790,7 +796,10 @@ describe('Profile.page', () => {
                     id: 'sample_cert_id',
                     url: 'https://sampleCertUrl.com',
                     token: 'AXOBC'
-                }
+                },
+                courseName: 'sample_course',
+                dateTime: '1333065600000',
+                status: 0
             });
             // assert
             setTimeout(() => {
@@ -898,10 +907,23 @@ describe('Profile.page', () => {
             mockCommonUtilService.networkInfo = {isNetworkAvailable: false};
             mockCertificateDownloadPdfService.download = jest.fn(() => Promise.resolve());
             jest.spyOn(profilePage, 'openpdf').mockImplementation();
-            mockCourseService.downloadCurrentProfileCourseCertificateV2 = jest.fn(() => of({path: 'sample_url'}));
             mockCertificateDownloadPdfService.download = jest.fn(() => Promise.resolve());
+            mockCourseService.certificateManager = {
+                    isCertificateCached: jest.fn(() => of(true))
+                }
             // act
-            profilePage.downloadTrainingCertificate({courseId: 'sample_cert_id', issuedCertificate: {id: 'sample_cert_id', name: 'sample_cert_name', token: 'ABSCD'}}, );
+            profilePage.downloadTrainingCertificate(
+                {
+                    courseId: 'sample_cert_id',
+                    issuedCertificate: {
+                        id: 'sample_cert_id',
+                        name: 'sample_cert_name',
+                        token: 'ABSCD'
+                    },
+                    courseName: 'sample_course',
+                    dateTime: '1333065600000',
+                    status: 0
+                });
             // assert
             setTimeout(() => {
                 expect(mockTelemetryGeneratorService.generateInteractTelemetry).toHaveBeenCalledWith(
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -540,15 +540,16 @@ export class ProfilePage implements OnInit {
           values);
 
         if (course.issuedCertificate) {
+          const request = { courseId: course.courseId, certificate: course.issuedCertificate };
           if (!this.commonUtilService.networkInfo.isNetworkAvailable) {
-            this.commonUtilService.showToast('NO_INTERNET_TITLE', false, '', 3000, 'top');
-            return;
+            if (!(await this.courseService.certificateManager.isCertificateCached(request).toPromise())) {
+              this.commonUtilService.showToast('NO_INTERNET_TITLE', false, '', 3000, 'top');
+              return;
+            }
           }
 
           this.router.navigate([`/${RouterLinks.PROFILE}/${RouterLinks.CERTIFICATE_VIEW}`], {
-            state: {
-              request: { courseId: course.courseId, certificate: course.issuedCertificate }
-            }
+            state: { request }
           });
         } else {
           const downloadMessage = await this.translate.get('CERTIFICATE_DOWNLOAD_INFO').toPromise();
