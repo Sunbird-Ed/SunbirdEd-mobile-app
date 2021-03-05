f669d0b1f3d6df854893cfcc90d948aeb4999771
Ajoymaity <ajoy.maity@tarento.com>
2021-03-05 14:58:36 +0530

    Issue #SB-0000 fix: joyful theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -961,7 +961,7 @@ export class AppComponent implements OnInit, AfterViewInit {
   async applyJoyfulTheme() {
     const isJoyfulThemePopupSeen = await this.preferences.getBoolean(PreferenceKey.COACH_MARK_SEEN).toPromise();
     if (!isJoyfulThemePopupSeen) {
-      await this.preferences.putString('current_selected_theme', 'JOYFUL').toPromise();
+      await this.preferences.putString('current_selected_theme', AppThemes.JOYFUL).toPromise();
       this.preferences.putBoolean(PreferenceKey.IS_JOYFUL_THEME_POPUP_DISPLAYED, true).toPromise().then();
       this.headerService.showStatusBar().then();
     }
