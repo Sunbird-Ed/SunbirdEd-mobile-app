02dd1e3aa8b2d21be374d497b4268b16a26995e0
swayangjit <swayangjit029@gmail.com>
2021-03-29 10:33:13 +0530

    Issue #SB-23829 test: Updated testcases.

--- src/services/common-util.service.spec.ts
+++ src/services/common-util.service.spec.ts
@@ -31,7 +31,9 @@ describe('CommonUtilService', () => {
     putString: jest.fn(() => of(undefined)),
   };
   const mockProfileService: Partial<ProfileService> = {};
-  const mockTelemetryService: Partial<TelemetryService> = {};
+  const mockTelemetryService: Partial<TelemetryService> = {
+    populateGlobalCorRelationData: jest.fn()
+  };
   const mockToastController: Partial<ToastController> = {
     create: jest.fn(() => (Promise.resolve({
       present: jest.fn(() => Promise.resolve({})),
@@ -627,6 +629,7 @@ describe('CommonUtilService', () => {
         dismiss: jest.fn(() => Promise.resolve({}))
       }) as any);
       mockTelemetryGeneratorService.generateInteractTelemetry = jest.fn();
+      mockNetwork.onChange = jest.fn(() => of([{ type: 'online' }]));
       // act
       commonUtilService.showExitPopUp('permission', 'home', false);
       // assert
@@ -642,6 +645,7 @@ describe('CommonUtilService', () => {
       commonUtilService = new CommonUtilService(
         mockSharedPreferences as SharedPreferences,
         mockProfileService as ProfileService,
+        mockTelemetryService as TelemetryService,
         mockTranslateService as TranslateService,
         mockLoadingController as LoadingController,
         mockEvents as Events,
