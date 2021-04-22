42c6469ae00a864cc65f223bdbe2464f7640545b
rejneesh1 <pillairejneesh@gmail.com>
2021-04-22 11:12:43 +0530

    SB-23776,Fix :offline msgs corrected, device id added in api call

--- src/app/manage-learn/core/services/dhiti-api.service.ts
+++ src/app/manage-learn/core/services/dhiti-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { AuthService } from '@project-sunbird/sunbird-sdk';
+import { AuthService, DeviceInfo } from '@project-sunbird/sunbird-sdk';
 import { ToastService, UtilsService } from '.';
 import { urlConstants } from '../constants/urlConstants';
 import { ApiUtilsService } from './api-utils.service';
@@ -17,9 +17,11 @@ export class DhitiApiService extends ApiService {
     public toast: ToastService,
     public modalController: ModalController,
     @Inject('AUTH_SERVICE') public authService: AuthService,
-     private utils: ApiUtilsService
+    @Inject('DEVICE_INFO') public deviceInfo: DeviceInfo,
+
+    private utils: ApiUtilsService
   ) {
-    super(http, toast, modalController,authService, utils);
-    this.baseUrl =  this.utils.getBaseUrl('assessmentBaseUrl')  + urlConstants.SERVICES.DHITI;
+    super(http, toast, modalController, authService,deviceInfo, utils);
+    this.baseUrl = this.utils.getBaseUrl('assessmentBaseUrl') + urlConstants.SERVICES.DHITI;
   }
 }
