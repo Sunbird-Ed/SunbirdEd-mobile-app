0acd6ad62f6bbdca9cf53fbf887fefeb07b769bb
rejneesh1 <pillairejneesh@gmail.com>
2021-04-23 11:10:37 +0530

    SB-23906:fix-viewAllAns button added for survey creator report

--- src/app/manage-learn/core/services/survey-provider.service.ts
+++ src/app/manage-learn/core/services/survey-provider.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { rejects } from 'assert';
 import { LoaderService, LocalStorageService, UtilsService } from '..';
 import { urlConstants } from '../constants/urlConstants';
@@ -9,6 +9,7 @@ import { AssessmentApiService } from './assessment-api.service';
 import { UpdateLocalSchoolDataService } from './update-local-school-data.service';
 import { SurveyMsgComponent } from '../../shared/components/survey-msg/survey-msg.component';
 import { storageKeys } from '../../storageKeys';
+import { DhitiApiService } from './dhiti-api.service';
 
 @Injectable({
   providedIn: 'root',
@@ -22,7 +23,9 @@ export class SurveyProviderService {
     private loader: LoaderService,
     private assessmentService: AssessmentApiService,
     private router: Router,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private dhiti: DhitiApiService,
+    private toast: ToastController
   ) {}
 
   // get all list
@@ -146,25 +149,26 @@ export class SurveyProviderService {
     await modal.present();
   }
 
-  //TODO
-  // viewAllAns(payload) {
-  //   let url = AppConfigs.surveyFeedback.getAllAnswers;
-
-  //   return new Promise((resolve, reject) => {
-  //     this.apiProvider.httpPost(
-  //       url,
-  //       payload,
-  //       (success) => {
-  //         this.utils.stopLoader();
-  //         resolve(success);
-  //       },
-  //       (error) => {
-  //         this.utils.openToast(error.message);
-  //         this.utils.stopLoader();
-  //         reject();
-  //       },
-  //       { baseUrl: "dhiti" }
-  //     );
-  //   });
-  // }
+  viewAllAns(payload) {
+    this.loader.startLoader()
+    let url = urlConstants.API_URLS.SURVEY_FEEDBACK.GET_ALL_ANSWERS 
+      const config = {
+        url: url,
+        payload: payload,
+      };
+    return new Promise((resolve, reject) => {
+      this.dhiti.post(
+       config).subscribe(
+        (success) => {
+          this.loader.stopLoader();
+          resolve(success);
+        },
+        (error) => {
+          this.toast.create(error.message);
+          this.loader.stopLoader();
+          reject();
+        },
+      );
+    });
+  }
 }
