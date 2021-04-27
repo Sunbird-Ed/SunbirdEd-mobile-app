c340411969836e5ec37e37550bd7f6e3edbd4a06
diptesh6501 <diptesh1993@gmail.com>
2021-04-27 16:25:34 +0530

    Issue #SB-23545 fix: addressed the issue where video player was not getting launched

--- src/app/player/player.page.ts
+++ src/app/player/player.page.ts
@@ -114,8 +114,8 @@ export class PlayerPage implements OnInit, OnDestroy, PlayerActionHandlerDelegat
       this.config['config'].sideMenu.showPrint = false;
        this.playerType = 'sunbird-quml-player';
     } else if(this.config['metadata']['mimeType'] === "video/mp4" && this.checkIsPlayerEnabled(this.playerConfig , 'videoPlayer').name === "videoPlayer"){
-      this.config['config'].sideMenu.showPrint = false;
       this.config = await this.getNewPlayerConfiguration();
+      this.config['config'].sideMenu.showPrint = false;
        this.playerType = 'sunbird-video-player';
     }
     this.config['context'].dispatcher = {
