56e843bac280b2401cb86a5cf093a4061d1e0606
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2021-04-27 14:43:21 +0530

    Issue #SB-22743 feat: adding video and fixing UI issues

---
+++ src/app/faq-help/faq-help-data.ts
@@ -0,0 +1,202 @@
+export const FaqSampleData = {
+  "categories": [
+    {
+      "name": "Login",
+      "videos": [
+        {
+          "id": "01",
+          "name": "Sample Video - 1",
+          "thumbnail": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+          "url": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4"
+        },
+        {
+          "id": "02",
+          "name": "Sample Video - 2",
+          "thumbnail": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+          "url": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4"
+        }
+      ],
+      "faqs": [
+        {
+          "topic": "I want to register on DIKSHA",
+          "description": "I want to register on DIKSHA"
+        },
+        {
+          "topic": "I want to login on DIKSHA through my State portal",
+          "description": "I want to login on DIKSHA through my State portal"
+        }
+      ]
+    },
+    {
+      "name": "Profile",
+      "videos": [
+
+      ],
+      "faqs": [
+        {
+          "topic": "I forgot my password",
+          "description": "I forgot my password"
+        },
+        {
+          "topic": "I want to edit my profile details",
+          "description": "I want to edit my profile details"
+        }
+      ]
+    },
+    {
+      "name": "Course & certificates",
+      "videos": [
+
+      ],
+      "faqs": [
+        {
+          "topic": "I want to download my certificate",
+          "description": "I want to download my certificate"
+        },
+        {
+          "topic": "I want to join a course",
+          "description": "I want to join a course"
+        },
+        {
+          "topic": " I want my course certificate soon after I complete my course ",
+          "description": " I want my course certificate soon after I complete my course "
+        },
+        {
+          "topic": "I want to check my course progress ",
+          "description": "I want to check my course progress "
+        },
+        {
+          "topic": "I want to check if a course provides a certificate",
+          "description": "I want to check if a course provides a certificate"
+        },
+        {
+          "topic": "I want to receive a notification after my certificate is issued",
+          "description": "I want to receive a notification after my certificate is issued"
+        },
+        {
+          "topic": "I want to change my name on the certificate issued to me",
+          "description": "I want to change my name on the certificate issued to me"
+        },
+        {
+          "topic": "I have completed my course, but the progress does not show as 100%  completed",
+          "description": "I have completed my course, but the progress does not show as 100%  completed"
+        },
+        {
+          "topic": "I want to contribute content to DIKSHA",
+          "description": "I want to contribute content to DIKSHA"
+        },
+        {
+          "topic": "I cannot find my course",
+          "description": "I cannot find my course"
+        }
+      ]
+    }
+  ],
+  "constants": {
+    "charactersLeft": "characters left",
+    "explainMsg": "Could you explain the problem?",
+    "faqMsg": "Frequently asked questions",
+    "help": "Help",
+    "helpMsg": "Did this answer help you?",
+    "initiateEmailButton": "Initiate email",
+    "knowMoreMsg": "What more would you like to know?",
+    "noMsg": "No",
+    "reportIssue": "Report an issue",
+    "reportIssueMsg": "Report other issue",
+    "resolveMsg": "Look through the list to resolve your issue",
+    "sorryMsg": "Sorry about that!",
+    "submitButton": "Submit feedback",
+    "tellMoreMsg": "Tell us more about the problem you faced",
+    "thanksMsg": "Thank you for your response!",
+    "triggerEmailMsg": "After you add details in the text box and tap the Initiate email button, we will trigger an email on your behalf. You can add further details and/or attach files there.",
+    "typeHere": "Type Here...",
+    "yesMsg": "Yes",
+    "selectCategoryTitle": "Category",
+    "videoTitle": "Video",
+    "faqTitle": "Faq",
+    "thanksForFeedbackMsg": "Thanks for your feedback. We may not be able to respond to every suggestion sent to us, but your feedback is valuable. It helps make {{app_name}} better for everyone."
+  }
+}
+
+export const VideoConfig = {
+  "context": {
+    "mode": "play",
+    "authToken": "",
+    "sid": "",
+    "did": "",
+    "uid": "",
+    "channel": "",
+    "pdata": { "id": "", "ver": "", "pid": "" },
+    "contextRollup": { "l1": "" },
+    "tags": [""],
+    "cdata": [],
+    "timeDiff": 0,
+    "objectRollup": {},
+    "host": "",
+    "endpoint": "",
+    "userData": {
+      "firstName": "",
+      "lastName": ""
+    }
+  },
+  "config": {
+    "traceId": ""
+  },
+  "metadata": {
+    "compatibilityLevel": 0,
+    "copyright": "",
+    "subject": [
+      ""
+    ],
+    "channel": "",
+    "language": [
+      ""
+    ],
+    "mimeType": "video/mp4",
+    "objectType": "Content",
+    "gradeLevel": [
+      ""
+    ],
+    "appIcon": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+    "primaryCategory": "",
+    "artifactUrl": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4",
+    "contentType": "",
+    "identifier": "",
+    "audience": [
+      ""
+    ],
+    "visibility": "Default",
+    "mediaType": "content",
+    "osId": "",
+    "languageCode": [
+      ""
+    ],
+    "license": "",
+    "name": "",
+    "status": "Live",
+    "code": "",
+    "streamingUrl": "https://ntpprodmedia-inct.streaming.media.azure.net/67a523f9-9590-4b5e-b161-bf6753a5e9d6/engagement-with-language-.ism/manifest(format=m3u8-aapl-v3)",
+    "medium": [
+      ""
+    ],
+    "createdOn": "2020-08-24T17:58:32.911+0000",
+    "copyrightYear": 2020,
+    "lastUpdatedOn": "2020-08-25T04:36:47.587+0000",
+    "creator": "",
+    "pkgVersion": 1,
+    "versionKey": "",
+    "framework": "",
+    "createdBy": "",
+    "resourceType": "Learn",
+    "orgDetails": {
+      "email": "",
+      "orgName": ""
+    },
+    "licenseDetails": {
+      "name": "",
+      "url": "",
+      "description": ""
+    }
+  },
+  "data": {}
+}
