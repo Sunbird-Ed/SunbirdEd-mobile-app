1fdae476d67c92640adb5ffd8944527f6b74c3c0
ArathyBC <arathybc@gmail.com>
2021-05-04 17:49:31 +0530

    Issue #SB 0000 test: added test cases for joyfulThemePopup Component

---
+++ src/app/components/popups/joyful-theme-popup/joyful-theme-popup.component.spec.ts
@@ -0,0 +1,104 @@
+import {JoyfulThemePopupComponent} from './joyful-theme-popup.component';
+import { Component, Inject, OnInit } from '@angular/core';
+import { NavParams, PopoverController } from '@ionic/angular';
+import { AppThemes } from '@app/app/app.constant';
+import { SharedPreferences } from 'sunbird-sdk';
+import { AppHeaderService } from '@app/services';
+import { of } from 'rxjs';
+
+describe("JoyfulThemePopupComponent", () => {
+    let joyfulThemePopupComponent : JoyfulThemePopupComponent ;
+    const mockPreferences : Partial<SharedPreferences> = {};
+    const mockPopoverController : Partial<PopoverController> = {};
+    const mockNavParams : Partial<NavParams> = {};
+    const mockAppHeaderService : Partial<AppHeaderService> = {};
+    
+    beforeAll(() => {
+        joyfulThemePopupComponent = new JoyfulThemePopupComponent(
+            mockPreferences as SharedPreferences,
+            mockPopoverController as PopoverController,
+            mockNavParams as NavParams,
+            mockAppHeaderService as AppHeaderService
+        );
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.resetAllMocks();
+    });
+
+    it('should create instance of JoyfulThemePopupComponent',() => {
+        expect(joyfulThemePopupComponent).toBeTruthy();
+    });
+
+    describe('ngOnit', () => {
+        it('should set values to appTheme and appName', (done) => {
+            //arrange
+            mockNavParams.get = jest.fn(() => ('appName'));
+            //act
+            joyfulThemePopupComponent.ngOnInit();
+            //assert
+            expect(joyfulThemePopupComponent.appName).toEqual('appName');
+            // expect(joyfulThemePopupComponent.appTheme).toEqual('');
+            setTimeout(() => {
+                expect(joyfulThemePopupComponent.isPopoverPresent).toEqual(true);
+                done();
+            },2000);
+        })
+    })
+
+    // describe('switchToJoyfulTheme', () => {
+    //     it('should switch default to joyful theme', (done) => {
+    //         //arrange
+    //         mockPreferences.putString = jest.fn(() => of());
+    //         mockAppHeaderService.showStatusBar = jest.fn(() =>
+    //          Promise.resolve() );
+    //         const appTheme = 'sample-app-theme';
+    //         // var document : Document;
+    //         // document.querySelector() = {
+    //         //     getAttribute : jest.fn(() => ('sample-result'))
+    //         // }
+
+    //         //act
+    //         joyfulThemePopupComponent.switchToJoyfulTheme()
+
+    //         //assert
+    //         setTimeout(() => {
+    //             // expect(mockPreferences.putString).toHaveBeenCalledWith(
+    //                 // 'current_selected_theme', appTheme);
+    //             expect(mockAppHeaderService.showStatusBar).toHaveBeenCalled();
+    //             done();
+    //         },0);
+    //     });
+    // });
+
+    describe('closePopover()', () => {
+        it('should switch to JoyfulTheme', (done) => {
+            //arrange
+            jest.spyOn(joyfulThemePopupComponent,'switchToJoyfulTheme').mockImplementation();
+            mockPopoverController.dismiss = jest.fn();
+            //act
+            joyfulThemePopupComponent.closePopover();
+            //assert
+            setTimeout(() => {
+                expect(mockPopoverController.dismiss).toHaveBeenCalled();
+                done();
+            },0);
+        })
+    });
+
+    describe('switchToNewTheme()', () => {
+        it('should switch to JoyfulTheme', (done) => {
+            //arrange
+            jest.spyOn(joyfulThemePopupComponent,'switchToJoyfulTheme').mockImplementation();
+            mockPopoverController.dismiss = jest.fn();
+            //act
+            joyfulThemePopupComponent.switchToNewTheme();
+            //assert
+            setTimeout(() => {
+                expect(mockPopoverController.dismiss).toHaveBeenCalled();
+                done();
+            },0);
+        })
+    })
+})
