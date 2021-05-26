342aa17f4a8cedc8ec7b712f5693c8b58db7963f
swayangjit <swayangjit029@gmail.com>
2021-05-26 12:29:01 +0530

    Issue #SB-000 chore: Updated search filter form

--- src/app/form.constants.ts
+++ src/app/form.constants.ts
@@ -26,7 +26,7 @@ export class FormConstants {
     public static  SEARCH_FILTER: FormRequest = {
         type: 'config',
         subType: 'search',
-        action: 'filter_v3',
+        action: 'filter_v2',
         component: 'app'
     };
 
