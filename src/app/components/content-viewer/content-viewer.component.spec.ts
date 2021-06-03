5cca2fc1b4521d954de060f36762a17089367438
sujith <sujith.sojan@tarento.com>
2021-06-03 13:58:14 +0530

    Issue #SB-0000 test:Updated the test cases for the content viewer component

--- src/app/components/content-viewer/content-viewer.component.spec.ts
+++ src/app/components/content-viewer/content-viewer.component.spec.ts
@@ -1,68 +1,63 @@
-import { ContentViewerComponent } from './content-viewer.component';
-import { ModalController } from '@ionic/angular';
-import {  Component, Input, OnInit } from '@angular/core';
-import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ContentViewerComponent } from "./content-viewer.component";
+import { ModalController } from "@ionic/angular";
+import { Component, Input, OnInit } from "@angular/core";
+import { ScreenOrientation } from "@ionic-native/screen-orientation/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
+describe("UserTypeSelectionPage", () => {
+  let contentViewerComponent: ContentViewerComponent;
+  const mockScreenOrientation: Partial<ScreenOrientation> = {
+    unlock: jest.fn(),
+    ORIENTATIONS: {
+      PORTRAIT: "PORTRAIT",
+      LANDSCAPE: "LANDSCAPE",
+    } as any,
+    lock: jest.fn(() => Promise.resolve([])),
+  };
+  const mockStatusBar: Partial<StatusBar> = {};
+  const mockModalController: Partial<ModalController> = {};
 
-describe('UserTypeSelectionPage', () => {
-    let  contentViewerComponent:  ContentViewerComponent;
-    const mockScreenOrientation: Partial<ScreenOrientation> = {
-        unlock: jest.fn(),
-        ORIENTATIONS: {
-            PORTRAIT: 'PORTRAIT' ,
-            LANDSCAPE: 'LANDSCAPE'
-
-        } as any,
-        lock: jest.fn(() => Promise.resolve([]))
-
-    };
-const mockStatusBar: Partial<StatusBar> = {};
-const mockModalController: Partial<ModalController> = {};
-
-beforeAll(() => {
+  beforeAll(() => {
     contentViewerComponent = new ContentViewerComponent(
-        mockScreenOrientation as ScreenOrientation,
-        mockStatusBar as StatusBar,
-        mockModalController as ModalController
+      mockScreenOrientation as ScreenOrientation,
+      mockStatusBar as StatusBar,
+      mockModalController as ModalController
     );
-});
-beforeEach(() => {
+  });
+  beforeEach(() => {
     jest.clearAllMocks();
     jest.resetAllMocks();
-});
+  });
 
-it('should be create a instance of ContentViewerComponent', () => {
+  it("should be create a instance of ContentViewerComponent", () => {
     expect(ContentViewerComponent).toBeTruthy();
-});
+  });
 
-it('should select ', () => {
+  it("ionViewWillLeave ", () => {
+    //arrange
     mockStatusBar.show = jest.fn();
-    mockScreenOrientation.unlock =jest.fn();
-    mockScreenOrientation.lock =jest.fn();
-
-
+    mockScreenOrientation.unlock = jest.fn();
+    mockScreenOrientation.lock = jest.fn();
+    //act
     contentViewerComponent.ionViewWillLeave();
-
+    //assert
     expect(mockStatusBar.show).toHaveBeenCalled();
     expect(mockScreenOrientation.unlock).toHaveBeenCalled();
-    expect(mockScreenOrientation.lock).toHaveBeenCalledWith(mockScreenOrientation.ORIENTATIONS.PORTRAIT);
-});
+    expect(mockScreenOrientation.lock).toHaveBeenCalledWith(
+      mockScreenOrientation.ORIENTATIONS.PORTRAIT
+    );
+  });
 
-it('should select ', () => {
+  it("ngOnInit ", () => {
+    //arrange
     mockStatusBar.hide = jest.fn();
-    mockScreenOrientation.lock =jest.fn();
-
-
+    mockScreenOrientation.lock = jest.fn();
+    //act
     contentViewerComponent.ngOnInit();
-
+    //assert
     expect(mockStatusBar.hide).toHaveBeenCalled();
-    expect(mockScreenOrientation.lock).toHaveBeenCalledWith(mockScreenOrientation.ORIENTATIONS.LANDSCAPE);
+    expect(mockScreenOrientation.lock).toHaveBeenCalledWith(
+      mockScreenOrientation.ORIENTATIONS.LANDSCAPE
+    );
+  });
 });
-
-
-
-});
-
-
-
