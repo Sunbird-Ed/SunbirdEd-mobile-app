8ac6ec48817026a5e2d8f6d2decc7e07e2873f5d
subranil <subranil.saha@tarento.com>
2021-07-01 18:48:33 +0530

    Issue #SB-25361 fix: audience information added and cc import changes

--- src/app/collection-detail-etb/collection-detail-etb.page.ts
+++ src/app/collection-detail-etb/collection-detail-etb.page.ts
@@ -19,7 +19,7 @@ import { ContentUtil } from '@app/util/content-util';
 import { IonContent as iContent, Platform, PopoverController } from '@ionic/angular';
 import { Events } from '@app/util/events';
 import { CsPrimaryCategory } from '@project-sunbird/client-services/services/content';
-import { ExpandBehavior, ExpandMode, IAccordianConfig, IButtonConfig, TocCardType } from '@project-sunbird/common-consumption-v8';
+import { ExpandBehavior, ExpandMode, IAccordianConfig, IButtonConfig, TocCardType } from '@project-sunbird/common-consumption';
 import isObject from 'lodash/isObject';
 import { Observable, Subscription } from 'rxjs';
 import { share } from 'rxjs/operators';
