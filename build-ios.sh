9736bb30733c983d6a4313c46c647249aa7e3105
kiran haridas <kiranharidas187@gmail.com>
2021-08-31 09:49:23 +0530

    Issue #0000:fix: Build and package changes

--- build-ios.sh
+++ build-ios.sh
@@ -61,6 +61,7 @@ npm install com.telerik.plugins.nativepagetransitions@0.7.0
 ionic cordova plugin rm cordova-plugin-sunbirdsplash
 ionic cordova plugin add cordova-plugin-splashscreen
 ionic cordova plugin rm cordova-plugin-fcm-with-dependecy-updated
+ionic cordova plugin rm cordova-plugin-file-support
 
 #Temporary Workaround to generate build as webpack was complaining of Heap Space
 #need to inspect on webpack dependdencies at the earliest
