0355abe07dc6e0a26248ec027733e65343e0e18f
subranil <subranil.saha@tarento.com>
2021-10-28 17:22:29 +0530

    Issue #SB-0000 test: added test-cases for components

---
+++ src/app/components/popups/sb-preferences-popup/sb-preferences-popup.component.spec.ts
@@ -0,0 +1,38 @@
+import {SbPreferencePopupComponent} from '@app/app/components/popups/sb-preferences-popup/sb-preferences-popup.component';
+import {ModalController} from '@ionic/angular';
+import {TelemetryGeneratorService} from '@app/services';
+import {AppVersion} from '@ionic-native/app-version/ngx';
+
+describe('SbPreferencesPopupComponent', () => {
+    let sbPreferencesPopupComponent: SbPreferencePopupComponent;
+    const mockModalController: Partial<ModalController> = {};
+    const mockAppVersion: Partial<AppVersion> = {
+        getAppName: jest.fn(() => Promise.resolve('Sunbird'))
+    };
+
+    beforeAll(() => {
+        sbPreferencesPopupComponent = new SbPreferencePopupComponent(
+            mockModalController as ModalController,
+            mockAppVersion as AppVersion
+        );
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should create instance of SbpreferenceComponent', () => {
+        // assert
+        expect(sbPreferencesPopupComponent).toBeTruthy();
+    });
+
+    it('should change preferences with returned object as true', () => {
+        // arrange
+        mockModalController.dismiss = jest.fn(() => {
+        }) as any;
+        // act
+        sbPreferencesPopupComponent.changePreference();
+        // assert
+        expect(mockModalController.dismiss).toHaveBeenCalledWith({showPreference: true});
+    });
+});
