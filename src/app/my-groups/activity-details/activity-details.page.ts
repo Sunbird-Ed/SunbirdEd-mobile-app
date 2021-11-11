6b6cd8ade9ece7fcdf073ceb8fe384016777122c
Swayangjit Prasad Parida <swayangjit029@gmail.com>
2021-11-11 15:53:00 +0530

    Revert "Issue #0000:fix: IOS issue fixes"

--- src/app/my-groups/activity-details/activity-details.page.ts
+++ src/app/my-groups/activity-details/activity-details.page.ts
@@ -248,20 +248,24 @@ export class ActivityDetailsPage implements OnInit, OnDestroy {
       });
     }
     return new Promise<boolean | undefined>(async (resolve) => {
-      const permissionStatus = await this.commonUtilService.getGivenPermissionStatus(AndroidPermission.WRITE_EXTERNAL_STORAGE);
-      if (permissionStatus.hasPermission) {
+      if (this.platform.is('ios')) {
         resolve(true);
-      } else if (permissionStatus.isPermissionAlwaysDenied) {
-        await this.commonUtilService.showSettingsPageToast('FILE_MANAGER_PERMISSION_DESCRIPTION', this.appName, PageId.PROFILE, true);
-        resolve(false);
       } else {
-        this.showStoragePermissionPopover().then((result) => {
-          if (result) {
-            resolve(true);
-          } else {
-            resolve(false);
-          }
-        });
+        const permissionStatus = await this.commonUtilService.getGivenPermissionStatus(AndroidPermission.WRITE_EXTERNAL_STORAGE);
+        if (permissionStatus.hasPermission) {
+          resolve(true);
+        } else if (permissionStatus.isPermissionAlwaysDenied) {
+          await this.commonUtilService.showSettingsPageToast('FILE_MANAGER_PERMISSION_DESCRIPTION', this.appName, PageId.PROFILE, true);
+          resolve(false);
+        } else {
+          this.showStoragePermissionPopover().then((result) => {
+            if (result) {
+              resolve(true);
+            } else {
+              resolve(false);
+            }
+          });
+        }
       }
     });
   }
