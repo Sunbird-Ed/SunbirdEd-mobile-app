a48bf401809c54428c1fcb7b82f6230f635639ae
afnantl <afnantl@tunerlabs.com>
2022-01-21 17:20:40 +0530

    SB-28283 edits

--- src/app/manage-learn/project/project-listing/project-listing.component.ts
+++ src/app/manage-learn/project/project-listing/project-listing.component.ts
@@ -69,7 +69,12 @@ export class ProjectListingComponent {
             this.translate.get(['FRMELEMNTS_LBL_ASSIGNED_TO_ME', 'FRMELEMNTS_LBL_CREATED_BY_ME','FRMELEMNTS_LBL_DISCOVERED_BY_ME']).subscribe(translations => {
             this.filters = [translations['FRMELEMNTS_LBL_ASSIGNED_TO_ME'], translations['FRMELEMNTS_LBL_DISCOVERED_BY_ME'], translations['FRMELEMNTS_LBL_CREATED_BY_ME']];
             });
-            this.selectedFilter = this.filters[0];
+            if( params.selectedFilter ){
+                this.selectedFilter = params.selectedFilter == 'assignedToMe' ? this.filters[0] : this.filters[1];
+                this.selectedFilterIndex = params.selectedFilter == 'assignedToMe' ? 0 : 1;
+            }else{
+                this.selectedFilter = this.filters[0];
+            }
         })
        
         this._networkSubscription = this.commonUtilService.networkAvailability$.subscribe(async (available: boolean) => {
