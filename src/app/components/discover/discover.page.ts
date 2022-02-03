713d097feff588ec0935f81e86c020a570d3901c
Naveen Kumar V <naveenkumarv@Netopss-MacBook-Pro.local>
2022-02-03 18:33:54 +0530

    Issue #SB-24823 feat: adding the configuration for facets

--- src/app/components/discover/discover.page.ts
+++ src/app/components/discover/discover.page.ts
@@ -143,6 +143,10 @@ export class DiscoverComponent implements OnInit, OnDestroy, OnTabViewWillEnter 
     if(section.dataSrc && section.dataSrc.params && section.dataSrc.params.config){
       const filterConfig = section.dataSrc.params.config.find(((facet) => (facet.type === 'filter' && facet.code === section.code)));
       event.data[0].value['primaryFacetFilters'] = filterConfig ? filterConfig.values : undefined;
+
+      const filterIdentifierList = section.dataSrc.params.config.find(((facet) => (facet.type === 'filterConfigIdentifier' && facet.code === section.code)));
+      const filterVal = filterIdentifierList && filterIdentifierList.values && filterIdentifierList.values.find(v=>v.code === event.data[0].name);
+      event.data[0].value['filterIdentifier'] = filterVal ? filterVal.filterIdentifier : undefined;
     }
     const params = {
       code: section.code,
