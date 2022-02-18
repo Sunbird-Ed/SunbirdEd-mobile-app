001f8e66a94775ed3ab45809aec34d60bdbc17ed
kiran haridas <kiranharidas187@gmail.com>
2022-02-18 19:15:35 +0530

    Issue #28868: fix: Issue fix

--- src/app/manage-learn/project/project-listing/project-listing.component.ts
+++ src/app/manage-learn/project/project-listing/project-listing.component.ts
@@ -246,11 +246,11 @@ export class ProjectListingComponent {
         this.kendra.post(config).subscribe(success => {
             this.loader.stopLoader();
             this.projects = this.projects.concat(success.result.data);
-            if (offilineIdsArr){
-                this.projects.map((p) => {
-                    if (offilineIdsArr.find((offProject) => offProject['_id'] == p._id)) p.downloaded = true;
-                });
-            }
+            // if (offilineIdsArr){
+            //     this.projects.map((p) => {
+            //         if (offilineIdsArr.find((offProject) => offProject['_id'] == p._id)) p.downloaded = true;
+            //     });
+            // }
             this.count = success.result.count;
             this.currentOnlineProjectLength = this.currentOnlineProjectLength + success.result.data.length;
             this.description = success.result.description;
