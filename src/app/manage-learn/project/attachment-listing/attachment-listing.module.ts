55610798766dc3094563fc3bb8724ffbc8a822e6
kiran haridas <kiranharidas187@gmail.com>
2022-03-21 11:23:21 +0530

    Issue #0000:fix: Prod build issue fix

--- src/app/manage-learn/project/attachment-listing/attachment-listing.module.ts
+++ src/app/manage-learn/project/attachment-listing/attachment-listing.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { AttachmentListingPageRoutingModule } from './attachment-listing-routing.module';
-
 import { AttachmentListingPage } from './attachment-listing.page';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AttachmentListingPageRoutingModule
+    AttachmentListingPageRoutingModule,
+    TranslateModule.forChild(),
+    SharedModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   declarations: [AttachmentListingPage]
 })
