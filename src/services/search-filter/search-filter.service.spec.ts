a38dd0421e4b5ba9c569371fe62420fb3c208740
SonaArun <sonavs2013@gmail.com>
2022-03-24 16:54:21 +0530

    Issue #SB-0000 test: Test cases for search-filter service updated

--- src/services/search-filter/search-filter.service.spec.ts
+++ src/services/search-filter/search-filter.service.spec.ts
@@ -1,4 +1,5 @@
 import { FormRequest } from '@project-sunbird/sunbird-sdk';
+import { throwError } from 'rxjs';
 import { FormAndFrameworkUtilService } from '../formandframeworkutil.service';
 import { SearchFilterService } from './search-filter.service';
 
@@ -22,7 +23,7 @@ describe('SearchFilterService', () => {
     });
 
     describe('fetchFacetFilterFormConfig', () => {
-        it('Should get Form Fields', (done) => {
+        it('Should get Form Fields by specifying the subtype', (done) => {
         //arrange
             const subType ="default";
             const formRequest: FormRequest = {action: "get",
@@ -122,6 +123,122 @@ describe('SearchFilterService', () => {
             done();
         });
     });
+
+    it('Should get Form Fields without specifying the subtype', (done) => {
+        //arrange
+            const formRequest: FormRequest = {action: "get",
+            component: "app",
+            subType: 'default',
+            type: "filterConfig"}; 
+            mockFormAndFrameworkUtilService.getFormFields = jest.fn(() => Promise.resolve([{
+                code: 'name',
+                default: 'default',
+                templateOptions: {
+                    hidden: false
+                }
+            }, {
+                code: 'persona',
+                templateOptions: {
+                    dataSrc: {
+                        marker: 'SUPPORTED_PERSONA_LIST',
+                        params: {
+                            useCase: 'sample-useCase'
+                        }
+                    },
+                    options: {}
+                },
+                children: {
+                    administrator: [
+                        {
+                            code: 'state',
+                            type: 'select',
+                            templateOptions: {
+                                labelHtml: {
+                                    contents: '<span>$0&nbsp;<span class=\'required-asterisk\'>*</span></span>',
+                                    values: {
+                                        $0: 'State'
+                                    }
+                                },
+                                placeHolder: 'Select State',
+                                multiple: false,
+                                dataSrc: {
+                                    marker: 'STATE_LOCATION_LIST',
+                                    params: {
+                                        useCase: 'SIGNEDIN_GUEST'
+                                    }
+                                }
+                            },
+                            validations: [
+                                {
+                                    type: 'required'
+                                }
+                            ]
+                        }, {
+                            code: 'subPersona',
+                            type: 'select',
+                            templateOptions: {
+                                labelHtml: {
+                                    contents: '<span>$0&nbsp;<span class=\'required-asterisk\'>*</span></span>',
+                                    values: {
+                                        $0: 'State'
+                                    }
+                                },
+                                placeHolder: 'Select State',
+                                multiple: false,
+                                dataSrc: {
+                                    marker: 'SUBPERSONA_LIST',
+                                    params: {
+                                        useCase: 'SIGNEDIN_GUEST'
+                                    }
+                                },
+                                options: { value: 'hm', label: 'HM' }
+                            }
+                        }, {
+                            code: 'district',
+                            type: 'select',
+                            templateOptions: {
+                                labelHtml: {
+                                    contents: '<span>$0&nbsp;<span class=\'required-asterisk\'>*</span></span>',
+                                    values: {
+                                        $0: 'State'
+                                    }
+                                },
+                                placeHolder: 'Select State',
+                                multiple: false,
+                                dataSrc: {
+                                    marker: 'LOCATION_LIST',
+                                    params: {
+                                        useCase: 'SIGNEDIN_GUEST'
+                                    }
+                                },
+                                options: { value: 'hm', label: 'HM' }
+                            }
+                        }]
+                }
+            }]));
+            //act
+            searchFilterService.fetchFacetFilterFormConfig().then(() => {
+            //assert
+            expect(mockFormAndFrameworkUtilService.getFormFields).toHaveBeenCalledWith(formRequest);
+            done();
+        });
+    });
+
+    it('Should get Form Fields and execute catch block', (done) => {
+        //arrange
+            const formRequest: FormRequest = {action: "get",
+            component: "app",
+            subType: 'default',
+            type: "filterConfig"}; 
+            mockFormAndFrameworkUtilService.getFormFields = jest.fn(() => Promise.reject({
+                error: 'Error message'                
+            }));
+            //act
+            const data = searchFilterService.fetchFacetFilterFormConfig().catch(() => {
+                expect(mockFormAndFrameworkUtilService.getFormFields).toHaveBeenCalledWith(formRequest);
+                done();
+            });
+    }); 
 }); 
 
     describe('getFacetFormAPIConfig', () => {
@@ -182,7 +299,7 @@ describe('reformFilterValues', () => {
             },
             {
                 name: 'se_gradeLevels',
-                value: [
+                values: [
                     {name: "class 1", apply: true},
                     {name: "lkg", apply: false},
                     {name: "ukg", apply: false},
@@ -193,10 +310,23 @@ describe('reformFilterValues', () => {
             }
         ];
         searchFilterService['facetFilterFormConfig'] = [
-            {code: "se_mediums", type: "dropdown", name: "language codes", placeholder: "Select Medium", multiple: true, index: 1},
-        {code: "se_gradeLevels", type: "dropdown", name: "language codes", placeholder: "Select Class", multiple: true, index: 2}
+            {code: "se_mediums", type: "dropdown", name: "language codes", placeholder: "Select Medium", multiple: true, index: 1,
+            value: [
+                {name: "english", count: 499, apply: true},
+                {name: "hindi", count: 8, apply: false},
+                {name: "sanskrit", count: 6, apply: false}
+            ]},
+        {code: "se_gradeLevels", type: "dropdown", name: "language codes", placeholder: "Select Class", multiple: true, index: 2,
+        values: [
+            {name: "class 1", apply: true},
+            {name: "lkg", apply: false},
+            {name: "ukg", apply: false},
+            {name: "class 4", apply: false},
+            {name: "class 5", apply: false},
+            {name: "class 6", apply: false}
+        ]}
         ];
-        const formAPIFacets = searchFilterService['facetFilterFormConfig'];
+        let formAPIFacets;
         searchFilterService.getFacetFormAPIConfig = jest.fn();
         //act
         searchFilterService.reformFilterValues(facetFilters, formAPIFacets).then(() => {
@@ -230,7 +360,7 @@ describe('reformFilterValues', () => {
             }
         ];
         searchFilterService['facetFilterFormConfig'] = null;
-        const formAPIFacets = searchFilterService['facetFilterFormConfig'];
+        let formAPIFacets;
         //act
         searchFilterService.reformFilterValues(facetFilters, formAPIFacets).then(() => {
         //assert
@@ -239,50 +369,4 @@ describe('reformFilterValues', () => {
         });
     });
 });
-
-describe('compareAndAssignValue', () => {
-    it('should return initialfacet if values is undefined', () => {
-    //arrange
-    const initialFacet = {name: "se_mediums", values: Array(3)}, 
-    replaceableFacet = {code: "se_mediums", type: "dropdown", name: "language codes", placeholder: "Select Medium", multiple: true, index: 1};
-    const compareAndAssignValue = jest.fn();
-    //act
-    compareAndAssignValue(initialFacet, replaceableFacet);
-    //assert
-    expect(compareAndAssignValue).toHaveBeenCalledWith(initialFacet,replaceableFacet);
-    });
-
-    it('should return initialfacet if values is defined', () => {
-    //arrange
-    const initialFacet = {name: "subject", values: [
-    {name: "general science", count: 1, apply: false},
-    {name: "physical science", count: 1, apply: false},
-    {name: "physical health and education", count: 1, apply: false},
-    {name: "meitei( manipuri )", count: 1, apply: false},
-    {name: "geography", count: 3, apply: false},
-    {name: "political science/civics", count: 1, apply: false},
-    {name: "english", count: 9, apply: false},
-    {name: "moya", count: 1, apply: false},
-    {name: "kokborok", count: 1, apply: false}
-    ]}, 
-    replaceableFacet = {code: "subject",
-    index: 3,
-    multiple: true,
-    name: "language names",
-    placeholder: "Select Subject",
-    type: "dropdown",
-    values: [
-        {name: "general science", apply: false},
-        {name: "physical science", apply: false},
-        {name: "physical health and education", apply: false},
-        {name: "meitei( manipuri )", apply: false},
-        {name: "geography", apply: false}
-    ]};
-    const compareAndAssignValue = jest.fn();
-    //act
-    compareAndAssignValue(initialFacet, replaceableFacet);
-    //assert
-    expect(compareAndAssignValue).toHaveBeenCalledWith(initialFacet,replaceableFacet);
-    });
-}); 
 });
