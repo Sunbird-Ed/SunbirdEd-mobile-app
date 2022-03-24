8b660dc49d15608c67521ca17c88737ea0bfb25e
Vishwa suresh <vishwasuresh1004@gmail.com>
2022-03-24 11:43:27 +0530

    Issue SB-29285 fix: Issue fixes

--- src/app/manage-learn/project/project.module.ts
+++ src/app/manage-learn/project/project.module.ts
@@ -25,7 +25,6 @@ import { ProjectTemplatePage } from './project-template/project-template.page';
 import { ItemListHeaderComponent } from './item-list-header/item-list-header.component'
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { AddFilePage } from './add-file/add-file.page';
-import { AttachmentListingPage } from './attachment-listing/attachment-listing.page';
 
 const routes: Routes = [
   {
@@ -65,7 +64,7 @@ const routes: Routes = [
   },
   {
     path: `${RouterLinks.ATTACHMENTS}/:id`,
-    component: AttachmentListingPage
+    loadChildren: './attachment-listing/attachment-listing.module#AttachmentListingPageModule'
   },
   {
     path: `${RouterLinks.TEMPLATE}/:id`,
@@ -83,7 +82,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ProjectDetailPage, ProjectListingComponent, ProjectEditPage, 
-    ProjectOperationPage, LearningResourcesPage, SyncPage, TaskViewPage, AttachmentListingPage,
+    ProjectOperationPage, LearningResourcesPage, SyncPage, TaskViewPage,
     LinkLearningResourcesComponent, AddEntityComponent, AddProgramsComponent, CreateProjectPage, 
     CategorySelectComponent,ProjectTemplatePage, ItemListHeaderComponent, ProjectDetailsComponent, AddFilePage],
   entryComponents: [LinkLearningResourcesComponent, AddEntityComponent, 
