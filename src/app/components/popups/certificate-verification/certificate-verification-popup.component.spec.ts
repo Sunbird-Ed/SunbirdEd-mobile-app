d6ac8b7300cf702a5a451d3f6551259c7e6214a1
Sathish Kumar <sathish.chandhiran@tarento.com>
2022-06-02 18:35:40 +0530

    Issue #SB-0000 test: Updated the test cases

---
+++ src/app/components/popups/certificate-verification/certificate-verification-popup.component.spec.ts
@@ -0,0 +1,82 @@
+import {CertificateVerificationPopoverComponent} from './certificate-verification-popup.component';
+import { PopoverController, Platform } from '@ionic/angular';
+import { CommonUtilService } from '../../../../services';
+import { Events } from '@app/util/events';
+
+describe('CertificateVerificationPopoverComponent', () => {
+
+    let certificateVerificationPopoverComponent: CertificateVerificationPopoverComponent;
+
+    const mockCommonUtilService: Partial<CommonUtilService> = {};
+    const mockPopOverCtrl: Partial<PopoverController> = {}; { };
+    const mockPlatform: Partial<Platform> = {};
+    const mockEvents: Partial<Events> = { publish: jest.fn() }; 
+
+    beforeAll(() => {
+        certificateVerificationPopoverComponent = new CertificateVerificationPopoverComponent(
+            mockCommonUtilService as CommonUtilService,
+            mockPopOverCtrl as PopoverController,
+            mockPlatform as Platform,
+            mockEvents as Events
+        );
+    }); 
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        jest.resetAllMocks();
+    });
+
+    it('should create a instance of certificateVerificationPopoverComponent', () => {
+        expect(certificateVerificationPopoverComponent).toBeTruthy();
+    }); 
+    
+    it('should subscribe to back button and events subscription', () => {
+        // arrange
+        const subscribeWithPriorityData = jest.fn((_, fn) => fn());
+        mockPopOverCtrl.dismiss = jest.fn();
+        mockPlatform.backButton = {
+            subscribeWithPriority: subscribeWithPriorityData,
+        } as any;
+
+        const unsubscribeFn = jest.fn();
+        certificateVerificationPopoverComponent.backButtonFunc = {
+            unsubscribe: unsubscribeFn,
+        } as any;
+        mockCommonUtilService.getAppName = jest.fn(() => Promise.resolve('sunbird')); 
+        // act
+        certificateVerificationPopoverComponent.ngOnInit();
+        // assert           
+        expect(mockCommonUtilService.getAppName).toHaveBeenCalled();
+        expect(mockPopOverCtrl.dismiss).toHaveBeenCalledWith({ isLeftButtonClicked: null });
+        expect(unsubscribeFn).toHaveBeenCalled();
+        
+    });
+ 
+    it('should unsubscribe to back button ngOnDestroy', () => {
+        // arrange
+        certificateVerificationPopoverComponent.backButtonFunc = {
+            unsubscribe: jest.fn(),
+        } as any;
+        // act
+        certificateVerificationPopoverComponent.ngOnDestroy();
+        // assert
+        expect(certificateVerificationPopoverComponent.backButtonFunc.unsubscribe).toHaveBeenCalled();
+    });
+    it('should close the popover on click', () => {
+    //arrange  
+     mockPopOverCtrl.dismiss = jest.fn();     
+    //act
+    certificateVerificationPopoverComponent.closePopover();
+    // assert 
+    expect(mockPopOverCtrl.dismiss).toHaveBeenCalledWith({ isLeftButtonClicked: null });
+    });
+
+    it('should close the popover on deleteContent', () => {
+        //arrange  
+         mockPopOverCtrl.dismiss = jest.fn();         
+        //act
+        certificateVerificationPopoverComponent.deleteContent(1);
+        // assert 
+        expect(mockPopOverCtrl.dismiss).toHaveBeenCalledWith({ isLeftButtonClicked: false });
+    });  
+});
