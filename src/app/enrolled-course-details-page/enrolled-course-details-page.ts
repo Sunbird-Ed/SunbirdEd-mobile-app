568c8f88c8e755c1a31370579cbc3dc1e9342f07
Ajoymaity <ajoy.maity@tarento.com>
2022-08-17 17:36:58 +0530

    Issue #SB-29020 fix: Fixed 1% difference in the course progress between mobile and portal

--- src/app/enrolled-course-details-page/enrolled-course-details-page.ts
+++ src/app/enrolled-course-details-page/enrolled-course-details-page.ts
@@ -897,6 +897,8 @@ export class EnrolledCourseDetailsPage implements OnInit, OnDestroy, ConsentPopo
             } else {
               this.isCertifiedCourse = false;
             }
+
+            
           }
           if (this.batchRemaningTimingIntervalRef) {
             clearInterval(this.batchRemaningTimingIntervalRef);
@@ -1988,7 +1990,7 @@ export class EnrolledCourseDetailsPage implements OnInit, OnDestroy, ConsentPopo
       if (this.courseHeirarchy.contentData.leafNodes) {
         leafNodeIds = this.courseHeirarchy.contentData.leafNodes;
       }
-      this.course.progress = Math.round((courseLevelViewedContents.length / leafNodeIds.length) * 100);
+      this.course.progress = Math.floor((courseLevelViewedContents.length / leafNodeIds.length) * 100);
     } else {
       this.course.progress = 0;
     }
