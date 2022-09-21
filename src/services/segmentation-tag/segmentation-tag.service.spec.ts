bd557b3621322c5a4cdebeb91170cac87422e9a3
Pavithra <pavithra.prakash@tarento.com>
2022-09-21 13:08:42 +0530

    Issue #SB-0000 test:"test cases fix"

--- src/services/segmentation-tag/segmentation-tag.service.spec.ts
+++ src/services/segmentation-tag/segmentation-tag.service.spec.ts
@@ -17,7 +17,7 @@ describe('SegmentationTagService ', () => {
         getTags: jest.fn(),
         saveTags: jest.fn(() => of(true)),
         getCommand: jest.fn(),
-        saveCommandList: jest.fn(() => of())
+        saveCommandList: jest.fn(() => of(true))
     };
     const mockProfileService: Partial<ProfileService> = {
         getActiveSessionProfile: jest.fn(() => of({uid: 'user_id'} as Profile))
@@ -77,14 +77,39 @@ describe('SegmentationTagService ', () => {
     });
 
     describe('presistTags', () => {
-        it('tags should be saved in db', (done) => {
+        it('tags and commandlist should be saved in db', (done) => {
             // arrnage
             jest.spyOn(mockSegmentationService, 'saveTags');
+            jest.spyOn(mockSegmentationService, 'saveCommandList');
             // act
             segmentationTagService.persistSegmentation();
             // assert
             setTimeout(() => {
                 expect(mockSegmentationService.saveTags).toBeCalled();
+                expect(mockSegmentationService.saveCommandList).toBeCalled();
+                done();
+            }, 100);
+        });
+        it('null tags and commandlist should be saved in db', (done) => {
+            // arrnage
+            jest.spyOn(mockSegmentationService, 'saveTags').mockImplementation(() => of(false));
+            jest.spyOn(mockSegmentationService, 'saveCommandList').mockImplementation(() => of(false));
+            // act
+            segmentationTagService.persistSegmentation();
+            // assert
+            setTimeout(() => {
+                expect(mockSegmentationService.saveTags).toBeCalled();
+                expect(mockSegmentationService.saveCommandList).toBeCalled();
+                done();
+            }, 100);
+        });
+        it('should checkfor profile with no uid', (done) => {
+            // arrnage
+            mockProfileService.getActiveSessionProfile = jest.fn(() => of())
+            // act
+            segmentationTagService.persistSegmentation();
+            // assert
+            setTimeout(() => {
                 done();
             }, 100);
         });
@@ -102,9 +127,12 @@ describe('SegmentationTagService ', () => {
             };
             const cmdStored = [
                 {
-                    cmommandId: 12387182678
+                    cmommandId: 12387182678,
+                    controlFunction: "DEBUGGING_MODE",
+                    controlFunctionPayload: {traceId: '6745'}
                 }
             ];
+            mockProfileService.getActiveSessionProfile = jest.fn(() => of({uid: "id"} as Profile))
             jest.spyOn(mockSegmentationService, 'getTags').mockReturnValue(of(JSON.stringify(tagsStored)));
             jest.spyOn(mockSegmentationService, 'getCommand').mockReturnValue(of(JSON.stringify(cmdStored)));
             mockFormAndFrameworkUtilService.getFormFields = jest.fn(() => Promise.resolve([{code: 'sample-code'}]));
@@ -119,7 +147,98 @@ describe('SegmentationTagService ', () => {
                 done();
             }, 100);
         });
+
+        it('get all tags and BANNER_CONFIG commands stored in db and and trigger the Local notification command', (done) => {
+            // arrnage
+            const tagsStored = {
+                __tagList: ['UA_English'],
+                __tagObj: [{name: 'Tags'}],
+                __tagSnapShot: {
+                    prifix_: {name: 'Tags'}
+                }
+            };
+            const cmdStored = [
+                {
+                    cmommandId: 12387152678,
+                    controlFunction: "BANNER_CONFIG",
+                    controlFunctionPayload: {showBanner: true}
+                }
+            ];
+            mockProfileService.getActiveSessionProfile = jest.fn(() => of({uid: "id"} as Profile))
+            jest.spyOn(mockSegmentationService, 'getTags').mockReturnValue(of(JSON.stringify(tagsStored)));
+            jest.spyOn(mockSegmentationService, 'getCommand').mockReturnValue(of(JSON.stringify(cmdStored)));
+            mockFormAndFrameworkUtilService.getFormFields = jest.fn(() => Promise.resolve([{code: 'sample-code'}]));
+            // act
+            segmentationTagService.getPersistedSegmentaion();
+            // assert
+            setTimeout(() => {
+                expect(mockSegmentationService.getTags).toBeCalled();
+                expect(mockSegmentationService.getCommand).toBeCalled();
+                expect(mockFormAndFrameworkUtilService.getFormFields).toHaveBeenCalled();
+                expect(mockNotificationSrc.setupLocalNotification).toBeCalled();
+                done();
+            }, 100);
+        });
+
+        it('get all tags and commands stored in db and and trigger the Local notification command for empty response', (done) => {
+            // arrnage
+            mockProfileService.getActiveSessionProfile = jest.fn(() => of({uid: "id"} as Profile))
+            jest.spyOn(mockSegmentationService, 'getTags').mockReturnValue(of(false));
+            jest.spyOn(mockSegmentationService, 'getCommand').mockReturnValue(of(false));
+            mockFormAndFrameworkUtilService.getFormFields = jest.fn(() => Promise.resolve());
+            // act
+            segmentationTagService.getPersistedSegmentaion();
+            // assert
+            setTimeout(() => {
+                expect(mockSegmentationService.getTags).toBeCalled();
+                expect(mockSegmentationService.getCommand).toBeCalled();
+                done();
+            }, 100);
+        });
+
+        it('should set else case for profile without uid', (done) => {
+            // arrnage
+            mockProfileService.getActiveSessionProfile = jest.fn(() => of())
+            // act
+            segmentationTagService.getPersistedSegmentaion();
+            // assert
+            setTimeout(() => {
+                done();
+            }, 100);
+        });
     });
+
+    describe('executeCommand', () => {
+        it('should execute Command for DEBUGGING_MODE', () => {
+            // arrange
+            const validCmdList = [{
+                cmommandId: 123871827778,
+                controlFunction: "DEBUGGING_MODE",
+                controlFunctionPayload: {traceId: '6745'}
+            }]
+            mockSharedPreferences.putString = jest.fn(() => of())
+            mockDebuggingService.enableDebugging = jest.fn(() => of(true));
+            mockEvent.publish = jest.fn();
+            // act
+            segmentationTagService.executeCommand(validCmdList)
+            // assert
+        })
+
+        it('should execute Command for Banner config', () => {
+            // arrange
+            const validCmdList = [{
+                cmommandId: 123871827978,
+                controlFunction: "BANNER_CONFIG",
+                controlFunctionPayload: {showBanner: true}
+            }]
+            mockSharedPreferences.putString = jest.fn(() => of())
+            mockDebuggingService.enableDebugging = jest.fn(() => of(true));
+            mockEvent.publish = jest.fn();
+            // act
+            segmentationTagService.executeCommand(validCmdList, false)
+            // assert
+        })
+    })
 
     describe('handleLocalNotificationTap', () => {
         it('handle actionable local notification', (done) => {
@@ -142,4 +261,24 @@ describe('SegmentationTagService ', () => {
             }, 0);
         });
     });
+
+    describe('createSegmentTags', () => {
+        it('should create SegmentTags', () => {
+            // arrange
+            const profile = {board:['cbse'], medium:["english"], grade:["class4"]};
+            // act
+            segmentationTagService.createSegmentTags(profile)
+            // assert
+        })
+    })
+
+    describe('refreshSegmentTags', () => {
+        it('should refresh SegmentTags', () => {
+            // arrange
+            const res = {board:[], medium:[], grade:[], syllabus: ""}
+            // act
+            segmentationTagService.refreshSegmentTags(res)
+            // assert
+        })
+    })
 });
