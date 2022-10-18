b6949daa4fcec7983dbe81e5307cdb47cd8338f5
Pavithra <pavithra.prakash@tarento.com>
2022-10-18 16:31:10 +0530

    Issue #ED-354 fix:"Migration fix to cordova-android10 - plugin updated"

--- scripts/android/android-10-migration-fix.js
+++ scripts/android/android-10-migration-fix.js
@@ -3,7 +3,9 @@
 const shell = require('child_process').execSync
 var pathList = [
     "platforms/android/cordova-plugin-badge/app-badge.gradle",
-    "platforms/android/com-sarriaroman-photoviewer/app-photoviewer.gradle"
+    "platforms/android/com-sarriaroman-photoviewer/app-photoviewer.gradle",
+    "platforms/android/cordova-plugin-code-push/app-build-extras.gradle",
+    "platforms/android/cordova-plugin-local-notification/app-localnotification.gradle",
 ]
 for (let i = 0; i < pathList.length; i++) {
     if(process.platform == "darwin") {
