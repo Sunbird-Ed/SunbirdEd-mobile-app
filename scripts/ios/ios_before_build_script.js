9187891f1a642421609e4eb13bc99a65958aefaa
kiran haridas <kiranharidas187@gmail.com>
2022-12-19 14:29:07 +0530

    Issue #0000:fix: iOS build automation

--- scripts/ios/ios_before_build_script.js
+++ scripts/ios/ios_before_build_script.js
@@ -3,39 +3,35 @@ var xml2js = require("xml2js");
 const fs = require('fs-extra');
 const propertiesReader = require('properties-reader');
 
-module.exports = function (context) {
-
-    (function () {
-        const configXmlPath = "config.xml";
-        const properties = propertiesReader('buildConfig/sunbird-ios.properties');
-        fs.readFile(configXmlPath, "utf-8", function (err, data) {
+(function () {
+    const configXmlPath = "config.xml";
+    const properties = propertiesReader('buildConfig/sunbird-ios.properties');
+    fs.readFile(configXmlPath, "utf-8", function (err, data) {
+        if (err) console.log(err);
+        parseString(data, function (err, result) {
             if (err) console.log(err);
-            parseString(data, function (err, result) {
-                if (err) console.log(err);
-                var json = result;
-                json.widget.$.id = properties.get('app_id')
-                json.widget.name = properties.get('app_name')
-                const deeplinkDomain = `applinks:${properties.get('deeplink_base_url')}`
-                for (const platform of json.widget.platform) {
-                    if (platform.$.name === 'ios') {
-                        if (platform["config-file"]) {
-                            for (const config of platform["config-file"]) {
-                                for (const domain of config.array) {
-                                    domain.string = [deeplinkDomain]
-                                }
+            var json = result;
+            json.widget.$.id = properties.get('app_id')
+            json.widget.name = properties.get('app_name')
+            const deeplinkDomain = `applinks:${properties.get('deeplink_base_url')}`
+            for (const platform of json.widget.platform) {
+                if (platform.$.name === 'ios') {
+                    if (platform["config-file"]) {
+                        for (const config of platform["config-file"]) {
+                            for (const domain of config.array) {
+                                domain.string = [deeplinkDomain]
                             }
                         }
                     }
                 }
-                var builder = new xml2js.Builder();
-                var xml = builder.buildObject(json);
+            }
+            var builder = new xml2js.Builder();
+            var xml = builder.buildObject(json);
 
-                fs.writeFile(configXmlPath, xml, function (err, data) {
-                    if (err) console.log(err);
-                    console.log("updated config.xml");
-                });
+            fs.writeFile(configXmlPath, xml, function (err, data) {
+                if (err) console.log(err);
+                console.log("updated config.xml");
             });
         });
-    })();
-
-}
+    });
+})();
