982c94b7842de079021bda6d354a98b388f7db08
Ajoymaity <ajoy.maity@tarento.com>
2023-02-06 13:20:16 +0530

    Issue #ED-1076 fix: Updated whitelistUrl in content config for player

--- src/app/player/player.page.ts
+++ src/app/player/player.page.ts
@@ -32,6 +32,7 @@ import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { ContentUtil } from '@app/util/content-util';
 import { PrintPdfService } from '@app/services/print-pdf/print-pdf.service';
+import { FormConstants } from '../form.constants';
 
 declare const cordova;
 
@@ -151,7 +152,7 @@ export class PlayerPage implements OnInit, OnDestroy, PlayerActionHandlerDelegat
     this.isExitPopupShown = false;
   }
   async ionViewWillEnter() {
-    const playerInterval = setInterval(() => {
+    const playerInterval = setInterval(async () => {
       if (this.playerType === 'sunbird-old-player') {
         this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
         this.statusBar.hide();
@@ -161,7 +162,9 @@ export class PlayerPage implements OnInit, OnDestroy, PlayerActionHandlerDelegat
         if (this.config['metadata'].isAvailableLocally) {
           this.config['metadata'].contentData.streamingUrl = '/_app_file_' + this.config['metadata'].contentData.streamingUrl;
         }
-
+        const utilityConfigFields = await this.formAndFrameworkUtilService.getFormFields(FormConstants.UTILITY_CONFIG);
+        this.config['config']['whiteListUrl'] = utilityConfigFields.find((config) =>
+              config.code === 'config')['config']['v1']['whitelistUrl'];
         if (this.previewElement?.nativeElement) {
           clearInterval(playerInterval);
           // This is to reload a iframe as iframes reload method not working on cross-origin.
