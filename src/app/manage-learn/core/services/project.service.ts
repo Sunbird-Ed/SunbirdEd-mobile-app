bbcf4f92390d9641960b3861375c4c03501e1f71
Vishwanath <vishwab@nfcsolutionsusa.com>
2023-03-24 15:44:52 +0530

    ISSUE #ED-1526 : Share consent pop-up is appearing a second time even if a user has already clicked on the 'SHARE' button from the consent pop-up for Discovered by me projects.

--- src/app/manage-learn/core/services/project.service.ts
+++ src/app/manage-learn/core/services/project.service.ts
@@ -123,6 +123,7 @@ export class ProjectService {
         replaceUrl: replaceUrl
       }
       this.db.create(success.result).then(successData => {
+        this.createNewProject(success.result, false)
         this.navigateToProjectDetails(navObj);
       }).catch(error => {
         if (error.status === 409) {
