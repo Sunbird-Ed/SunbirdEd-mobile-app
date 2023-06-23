ec64dc8c03cc8a30fc6785fd1417a947cc282af0
Vishwanath <vishwab@nfcsolutionsusa.com>
2023-06-23 09:14:19 +0530

    Issue #ED-2319 fix : Trail and error for labels code revert

--- src/app/manage-learn/programs/program-details/program-details.component.ts
+++ src/app/manage-learn/programs/program-details/program-details.component.ts
@@ -12,8 +12,6 @@ import { SurveyProviderService } from '../../core/services/survey-provider.servi
 import { UpdateLocalSchoolDataService } from '../../core/services/update-local-school-data.service';
 import { storageKeys } from '../../storageKeys';
 import { CommonUtilService } from '../../../../services/common-util.service';
-
-import * as strings from '../../../../assets/i18n/en.json'
 @Component({
   selector: 'app-program-details',
   templateUrl: './program-details.component.html',
@@ -60,7 +58,6 @@ export class ProgramDetailsComponent implements OnInit {
   ngOnInit() {}
 
   ionViewWillEnter(){
-    console.log(strings,"strings")
     this.headerConfig = this.headerService.getDefaultPageConfig()
     this.headerConfig.showHeader = true
     this.headerConfig.showBurgerMenu = false
