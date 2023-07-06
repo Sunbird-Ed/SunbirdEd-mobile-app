cbf42417e82d7cfa5e145b6b339959755c5c83e4
Vishwanath <vishwab@nfcsolutionsusa.com>
2023-07-06 18:13:54 +0530

    Issue #ED-2543 fix : QA issue fix

--- src/app/manage-learn/programs/program-details/program-details.component.ts
+++ src/app/manage-learn/programs/program-details/program-details.component.ts
@@ -31,7 +31,7 @@ export class ProgramDetailsComponent implements OnInit {
   showMore:boolean=false
   description
   characterLimit = 150
-  programDetails:any={}
+  programDetails:any={};
   solutionsList:any=[]
   filteredList:any=[]
   sharingStatus:any='REVOKED'
@@ -312,6 +312,10 @@ export class ProgramDetailsComponent implements OnInit {
     this.surveyProvider
       .getDetailsById(surveyId, data._id)
       .then((res) => {
+        if (!res.result) {
+          this.surveyProvider.showMsg('surveyExpired');
+          return;
+        }
         if (res.result && res.result.status == 'completed') {
           // this.toast.openToast(res.message)
           this.surveyProvider.showMsg('surveyCompleted');
