8e9c61a77b5f2e9aee11b36a882e4fc8f56d7dc5
vpPavithra <pavithra.prakash@tarento.com>
2024-04-02 22:35:21 +0530

    Issue #ED-3963 fix- build config fix

--- build.sh
+++ build.sh
@@ -4,34 +4,34 @@
 if [[ -f configurations/config.properties ]]; then 
     echo "File exists"
     # Simple script to clean install
-    # rm -rf node_modules
-    # rm -rf android
-    # rm -rf www
-    # rm package-lock.json
+    rm -rf node_modules
+    rm -rf android
+    rm -rf www
+    rm package-lock.json
 
-    # npm install
+    npm install
 
-    # # Read properties from config.properties
-    # if [[ "$(uname)" == "Darwin" ]] || [[ "$(uname)" == "Linux" ]]; then
-    #     APP_NAME=$(grep 'app_name' configurations/config.properties | cut -d'=' -f2)
-    #     APP_ID=$(grep 'app_id' configurations/config.properties | cut -d'=' -f2)
-    # else
-    #     APP_NAME=$(powershell.exe -Command "(Get-Content -Path 'configurations\config.properties' | Select-String 'app_name').ToString().Split('=')[1].Trim()")
-    #     APP_ID=$(powershell.exe -Command "(Get-Content -Path 'configurations\config.properties' | Select-String 'app_id').ToString().Split('=')[1].Trim()")
-    # fi
+    # Read properties from config.properties
+    if [[ "$(uname)" == "Darwin" ]] || [[ "$(uname)" == "Linux" ]]; then
+        APP_NAME=$(grep 'app_name' configurations/config.properties | cut -d'=' -f2)
+        APP_ID=$(grep 'app_id' configurations/config.properties | cut -d'=' -f2)
+    else
+        APP_NAME=$(powershell.exe -Command "(Get-Content -Path 'configurations\config.properties' | Select-String 'app_name').ToString().Split('=')[1].Trim()")
+        APP_ID=$(powershell.exe -Command "(Get-Content -Path 'configurations\config.properties' | Select-String 'app_id').ToString().Split('=')[1].Trim()")
+    fi
 
-    # # Update capacitor.config.ts
-    # sed -i'' -e "s/'app.name'/'$APP_NAME'/" capacitor.config.ts
-    # sed -i'' -e "s/'app.id'/'$APP_ID'/" capacitor.config.ts
+    # Update capacitor.config.ts
+    sed -i'' -e "s/'app.name'/'$APP_NAME'/" capacitor.config.ts
+    sed -i'' -e "s/'app.id'/'$APP_ID'/" capacitor.config.ts
 
-    # echo "updated appname and appid"
+    echo "updated appname and appid"
 
-    # Build your Ionic app
-    # ionic build
+    Build your Ionic app
+    ionic build
 
-    # Build your Ionic app, add android, generate icons and build
-    # npx cap add android
-    # appIcon
+    Build your Ionic app, add android, generate icons and build
+    npx cap add android
+    appIcon
     node hooks/uploadAppIcon.js
     npx @capacitor/assets generate --iconBackgroundColor '#ffffff' --iconBackgroundColorDark '#222222' --splashBackgroundColor '#ffffff' --splashBackgroundColorDark '#111111'
     
