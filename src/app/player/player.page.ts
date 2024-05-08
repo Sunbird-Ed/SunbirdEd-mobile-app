7b7f3ede84100d609a8f62cf9c5004dacf2213fa
Sujith <sujithsojan@gmail.com>
2024-05-08 16:01:52 +0530

    Issue #ED-4228 fix: User is not getting any message once the attempts are exhausted for questionset

--- src/app/player/player.page.ts
+++ src/app/player/player.page.ts
@@ -392,13 +392,24 @@ export class PlayerPage implements OnInit, OnDestroy, PlayerActionHandlerDelegat
       const userId: string = this.appGlobalService.getCurrentUser().uid;
       const parentId: string = (this.content.rollup && this.content.rollup.l1) ? this.content.rollup.l1 : this.content.identifier;
       const contentId: string = this.content.identifier;
-      if(event.detail.edata['type'] === 'EXIT') {
+      if (event.detail.edata['type'] === 'exdata') {
+        if (event.detail.edata['currentattempt']) {
+          const attemptInfo = {
+            isContentDisabled: event.detail.edata['maxLimitExceeded'],
+            isLastAttempt: event.detail.edata['isLastAttempt']
+          };
+          await this.commonUtilService.handleAssessmentStatus(attemptInfo);
+        }
+      }
+       else if(event.detail.edata['type'] === 'EXIT') {
         this.playerService.deletePlayerSaveState(userId, parentId, contentId);
         if (this.config['metadata']['mimeType'] === "application/vnd.sunbird.questionset") {
           if (!this.isExitPopupShown) {
             await this.showConfirm();
           }
-        } else {
+        }
+
+         else {
           this.location.back();
         }
       }
@@ -770,6 +781,7 @@ export class PlayerPage implements OnInit, OnDestroy, PlayerActionHandlerDelegat
         config: this.config.config,
         metadata: this.config.metadata
       };  
+      console.log(playerConfig,'playerConfig');
        
       setTimeout(() => {
         const qumlElement = document.createElement('sunbird-quml-player');
