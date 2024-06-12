6683ef54d7a21bbbf13a95e13b37a65b0fcde44e
vpPavithra <pavithra.prakash@tarento.com>
2024-06-12 18:58:37 +0530

    Issue #ED-0000 feat: update git actions flow and config file

--- scripts/configureGradleFiles.js
+++ scripts/configureGradleFiles.js
@@ -4,7 +4,9 @@ var readline = require('readline');
 let filePath = '';
 if (process.env.NODE_ENV == "staging") {
     filePath = 'configurations/configuration.stag.ts';
-} if (process.env.NODE_ENV == "production") {
+} else if (process.env.NODE_ENV == "production") {
+    filePath = 'configurations/configuration.prod.ts';
+} else {
     filePath = 'configurations/configuration.prod.ts';
 }
 const properties = {}
